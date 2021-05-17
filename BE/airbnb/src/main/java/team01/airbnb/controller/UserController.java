ad1a3b1f5f093292834d65577e5dd9076facd577
Shion <ehdrhelr@gmail.com>
2021-05-17 20:01:53 +0900

    [#3] feature : 카카오 로그인 토큰으로 사용자 정보 가져오기 구현

--- BE/airbnb/src/main/java/team01/airbnb/controller/UserController.java
+++ BE/airbnb/src/main/java/team01/airbnb/controller/UserController.java
@@ -1,5 +1,7 @@
 package team01.airbnb.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,6 +12,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import team01.airbnb.domain.OAuthToken;
 
 @Controller
 public class UserController {
@@ -44,6 +47,35 @@ public class UserController {
                 String.class
         );
 
-        return "카카오 토큰 요청 완료! 토큰 요청에 대한 응답 : " + response;
+        // Json 데이터를 자바 오브젝트로 처리
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = null;
+        try {
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
+
+        RestTemplate rt2 = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 = new HttpEntity<>(headers2);
+
+        // Http 요청하기 - Post 방식으로 - 그리고 response 변수의 응답 받음
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest2,
+                String.class
+        );
+
+        return response2.getBody();
     }
 }
