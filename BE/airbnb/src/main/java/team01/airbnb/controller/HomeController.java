8a8cfe709405ba4b21e1c6bf56416e4a2a035b2e
Shion <ehdrhelr@gmail.com>
2021-05-17 18:34:58 +0900

    [#2] chore : 서버 배포

---
+++ BE/airbnb/src/main/java/team01/airbnb/controller/HomeController.java
@@ -0,0 +1,13 @@
+package team01.airbnb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
+}
