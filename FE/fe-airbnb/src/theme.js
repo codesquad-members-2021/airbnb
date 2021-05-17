6198e14e1635f3203ea3beba13474041075b67bc
Hongbi <dyongdi@gmail.com>
2021-05-18 01:43:10 +0900

    add: style 관련 변수 정의

---
+++ FE/fe-airbnb/src/theme.js
@@ -0,0 +1,32 @@
+const theme = {
+  colors: {
+    black: '#010101',
+    gray1: '#333',
+    gray2: '#4F4F4F',
+    gray3: '#828282',
+    gray4: '#BDBDBD',
+    gray5: '#E0E0E0',
+    gray6: '#F5F5F7',
+    white: '#FFF',
+    pink: '#E84C60',
+    green: '#118917',
+  },
+  fontSizes: {
+    XL: '32px',
+    L: '20px',
+    M: '18px',
+    SM: '16px',
+    S: '14px',
+    XS: '12px',
+  },
+  borders: {
+    S: '10px',
+    L: '40px',
+  },
+  boxShadow: {
+    up: 'box-shadow: 0px 0px 4px rgba(204, 204, 204, 0.5), 0px 2px 4px rgba(0, 0, 0, 0.25)',
+    up2: '0px 4px 10px rgba(51, 51, 51, 0.1), 0px 0px 4px rgba(51, 51, 51, 0.05)',
+  },
+};
+
+export default theme;
