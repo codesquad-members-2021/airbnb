7f81e6ad2601e53e4071316770840eb8aa4b17a7
pbg0205 <pbg0205@naver.com>
2021-05-17 17:01:04 +0900

    feat : BE-project-file을 생성하라.

---
+++ BE/src/main/java/com/codsquad/airbnb/AirBnbApplication.java
@@ -0,0 +1,13 @@
+package com.codsquad.airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirBnbApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/com/codsquad/airbnb/AirBnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codsquad.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
