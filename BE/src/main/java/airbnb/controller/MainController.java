8d661f90e76180c2a1565ca8fbe4eec10991f711
95degree <95degree@naver.com>
2021-05-18 16:56:03 +0900

    feat: ✨ObjectMapper 설정(#5)
    @JsonRootName을 사용하기 위해 objectMapper 설정

--- BE/src/main/java/airbnb/controller/MainController.java
+++ BE/src/main/java/airbnb/controller/MainController.java
@@ -1,6 +1,8 @@
 package airbnb.controller;
 
 import airbnb.MainMockResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,8 @@ public class MainController {
 
     @GetMapping("/main")
     public MainMockResponse showMain(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
         return new MainMockResponse();
     }
 }
