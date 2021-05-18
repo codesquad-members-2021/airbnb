8889ae61245ab793fd8d6bda19b56d4c08b45781
zeke <yangsin1031@naver.com>
2021-05-18 11:46:50 +0900

    Feat: navigationBar Item 추가 및 설정

--- ios/airbnb/airbnb/Controller/LocationViewController.swift
+++ ios/airbnb/airbnb/Controller/LocationViewController.swift
@@ -7,18 +7,34 @@
 
 import UIKit
 
-class LocationViewController: UIViewController {
+final class LocationViewController: UIViewController {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationItem.backBarButtonItem?.title = "뒤로"
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.hidesNavigationBarDuringPresentation = false
-        self.navigationItem.searchController = searchController
-        self.navigationItem.title = "숙소 찾기"
+        configureNavigationItem()
+        configureSearchBar()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = false
+    }
+    
+    private func configureNavigationItem() {
+        navigationController?.navigationItem.backBarButtonItem?.title = "뒤로"
+        self.navigationItem.title = "숙소 찾기"
+        let cancel = UIBarButtonItem(title: "지우기", style: .done, target: self, action: #selector(tappedDeleteBarButton))
+        self.navigationItem.rightBarButtonItem = cancel
+    }
+    
+    private func configureSearchBar() {
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.automaticallyShowsCancelButton = false
+        self.navigationItem.searchController = searchController
+    }
+    
+    @objc func tappedDeleteBarButton() {
+        
     }
 }
