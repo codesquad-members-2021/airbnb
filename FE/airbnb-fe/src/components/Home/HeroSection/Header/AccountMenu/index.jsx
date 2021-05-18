dbca28bddec354cfcf812641e3ae2595f18d96bd
Jungwon Lee <leejungwon96@gmail.com>
2021-05-18 17:54:13 +0900

    Feat. 상단헤더 배치 d-h-k/Feature/#10/homeHeader - 로고 svg 컴포넌트화하기 - 아이콘 배치

---
+++ FE/airbnb-fe/src/components/Home/HeroSection/Header/AccountMenu/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AccountIcon from './AccountIcon.js';
+import MenuIcon from './MenuIcon.js';
+
+const AccountMenu = () => {
+  return (
+    <div>
+      <MenuIcon />
+      <AccountIcon />
+    </div>
+  );
+};
+
+export default AccountMenu;
--- FE/airbnb-fe/src/components/Home/HeroSection/Header/index.jsx
+++ FE/airbnb-fe/src/components/Home/HeroSection/Header/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import AccountMenu from './AccountMenu';
+import NavRadio from './NavRadio';
+import AirbnbIcon from './AirbnbIcon';
 
 const Header = () => {
-  return <HeaderDiv></HeaderDiv>;
+  return (
+    <HeaderDiv>
+      <AirbnbIcon />
+      <NavRadio />
+      <AccountMenu />
+    </HeaderDiv>
+  );
 };
 
 const HeaderDiv = styled.div`
   height: 94px;
+  display: flex;
+  justify-content: space-between;
+  width: 88.88%;
+  border: 1px solid red;
+  margin: 0 auto;
+  align-items: center;
   /* background-color: ${({ theme }) => theme.colors.gray3}; */
+`;
+
+const LogoIcon = styled.svg`
+  fill: black;
 `;
 
 export default Header;
