7dd656bba2d072b938da9f26581b5b54a7082a7a
JinseoPark <rarebook92@naver.com>
2021-05-18 17:14:41 +0900

    fix :bug: solve delete unwanted folders

---
+++ iOS/airBnB/airBnB/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  airBnB
+//
+//  Created by jinseo park on 5/17/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ iOS/airBnB/airBnB/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  airBnB
+//
+//  Created by jinseo park on 5/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+
+}
+
