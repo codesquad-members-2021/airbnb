ceab4b6c27e145540fb6ccf60862b437fe1d39a0
JinseoPark <rarebook92@naver.com>
2021-05-18 17:04:46 +0900

    feat: :sparkles: Create Project with gitignore

---
+++ airBnB/airBnB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airBnB
+//
+//  Created by jinseo park on 5/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS/airBnB/airBnB/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  airBnB
+//
+//  Created by jinseo park on 5/17/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
