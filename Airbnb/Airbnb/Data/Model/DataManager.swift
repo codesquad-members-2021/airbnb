8643beda8df73b6540ba435d1206b7d30697ea8b
Ray suh <readingod@naver.com>
2021-05-18 17:29:07 +0900

    feat: Add DataManager for MainView (#3)

---
+++ Airbnb/Airbnb/Data/Model/DataManager.swift
@@ -0,0 +1,44 @@
+enum ImageManager {
+    static let main = "main"
+    static let seoul = "seoul"
+    static let gwangju = "gwangju"
+    static let uijeongbu = "uijeongbu"
+    static let suwon = "suwon"
+    static let daegu = "daegu"
+    static let ulsan = "ulsan"
+    static let daejeon = "daejeon"
+    static let bucheon = "bucheon"
+    static let nature = "nature"
+    static let unique = "unique"
+}
+
+enum MainInfoManager {
+    static let main = "에어비앤비가 엄선한 위시리스트를 만나보세요."
+    static let seoul = "서울"
+    static let gwangju = "광주"
+    static let uijeongbu = "의정부시"
+    static let suwon = "수원시"
+    static let daegu = "대구"
+    static let ulsan = "울산"
+    static let daejeon = "대전"
+    static let bucheon = "부천시"
+    static let nature = "자연생활을 만끽할 수 있는 숙소"
+    static let unique = "독특한 공간"
+}
+
+enum HeaderManager {
+    static let first = "슬기로운 자연생활"
+    static let second = "가까운 여행지 둘러보기"
+    static let third = "어디에서나, 여행은 살아보는거야!"
+}
+
+enum DetailInfoManager {
+    static let seoul = "30분"
+    static let gwangju = "4시간"
+    static let uijeongbu = "30분"
+    static let suwon = "45분"
+    static let daegu = "3.5시간"
+    static let ulsan = "4.5시간"
+    static let daejeon = "2시간"
+    static let bucheon = "30분"
+}
