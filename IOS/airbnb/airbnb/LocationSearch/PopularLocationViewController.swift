6395ed0556ff52db6111008c5161e95e40d04f59
Song Lee <songlish233@gmail.com>
2021-05-18 16:31:59 +0900

    feat: 근처의 인기 여행지 리스트 화면 구성
    - 헤더, 셀 추가
    - 더미 데이터 구성
    
    Dae-Hwa/airbnb/#6

--- IOS/airbnb/airbnb/LocationSearch/PopularLocationViewController.swift
+++ IOS/airbnb/airbnb/LocationSearch/PopularLocationViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class PopularLocationViewController: UIViewController {
 
+    @IBOutlet weak var locationSearchBar: UISearchBar!
+    @IBOutlet weak var popularLocationTableView: UITableView!
+    
     static var reuseIdentifier: String {
         return String(describing: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationSearchBar.searchTextField.becomeFirstResponder()
+        self.popularLocationTableView.dataSource = self
+    }
+    
+}
 
+extension PopularLocationViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID = PopularLocationTableViewCell.reuseIdentifier
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as? PopularLocationTableViewCell ?? PopularLocationTableViewCell()
+        return cell
     }
     
 }
