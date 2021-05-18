eac7842dddb0d575a98f0d0b4ebcfb9d63dcf054
95degree <95degree@naver.com>
2021-05-18 18:44:03 +0900

    feat: ✨Get Mock Api 생성(#5)

--- BE/src/main/java/airbnb/MainMockResponse.java
+++ BE/src/main/java/airbnb/MainMockResponse.java
@@ -1,5 +1,31 @@
 package airbnb;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import java.util.List;
+
+@JsonRootName(value = "mainPage")
 public class MainMockResponse {
-    
+
+    private String hiroImage;
+    private List<CityResponse> cities;
+    private List<Category> categories;
+
+    public MainMockResponse(String hiroImage, List<CityResponse> cities, List<Category> categories) {
+        this.hiroImage = hiroImage;
+        this.cities = cities;
+        this.categories = categories;
+    }
+
+    public String getHiroImage() {
+        return hiroImage;
+    }
+
+    public List<CityResponse> getCities() {
+        return cities;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
 }
