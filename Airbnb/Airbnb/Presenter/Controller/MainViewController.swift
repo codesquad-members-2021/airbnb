6d0aff13040779bba3eea7873c1eccb9acf38cbb
Ray suh <readingod@naver.com>
2021-05-18 17:00:32 +0900

    feat: Implement Model For MainView (#3)

--- Airbnb/Airbnb/Presenter/Controller/MainViewController.swift
+++ Airbnb/Airbnb/Presenter/Controller/MainViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var travelSearchBar: UISearchBar!
     @IBOutlet weak var mainInfoCollectionView: UICollectionView!
