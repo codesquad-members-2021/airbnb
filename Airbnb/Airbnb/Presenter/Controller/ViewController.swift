4d8d108d4e9a072223f6d1728d4f5ade7e11e2a5
Ray suh <readingod@naver.com>
2021-05-18 16:26:19 +0900

    feat: Add SearchBar & MainCollectionView (#3)

---
+++ Airbnb/Airbnb/Presenter/Controller/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var travelSearchBar: UISearchBar!
+    @IBOutlet weak var mainInfoCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
