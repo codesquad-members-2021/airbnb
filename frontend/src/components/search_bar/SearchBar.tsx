7091592081d568026cff06316ad5196d4143adec
junzero741 <junzero741@gmail.com>
2021-05-18 23:04:07 +0900

    feat: 검색창 정적 UI 추가

--- frontend/src/components/search_bar/SearchBar.tsx
+++ frontend/src/components/search_bar/SearchBar.tsx
@@ -6,7 +6,8 @@ export interface IAppProps {}
 export default function SearchBar(props: IAppProps) {
 	return (
 		<StyleSearchBar>
-			<SearchBy type="위치" input="어디로 여행가세요?" isEnd={false} />
+			{/* 검색 조건에 지역 넣게되면 아래 줄 주석 풀기 */}
+			{/* <SearchBy type="위치" input="어디로 여행가세요?" isEnd={false} /> */} 
 			<SearchBy type="체크인" isEnd={false} placeholder="날짜 입력" />
 			<SearchBy type="체크아웃" isEnd={false} placeholder="날짜 입력" />
 			<SearchBy type="요금" isEnd={false} placeholder="금액대 설정" />
@@ -19,10 +20,11 @@ const StyleSearchBar = styled.div`
 	display: flex;
 	position: absolute;
 	align-items: center;
-	width: 945px;
+	justify-content: space-between;
+	width: 916px;
 	height: 76px;
 	top: 110px;
-	left: 500px;
+	left: 270px;
 	border-radius: 60px;
 	background-color: #ffffff;
 `;
--- frontend/src/components/search_bar/SearchBy.tsx
+++ frontend/src/components/search_bar/SearchBy.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {ReactComponent as SearchIcon} from './../../icons/search.svg';
 
 export interface IAppProps {
 	type: string;
@@ -8,21 +9,28 @@ export interface IAppProps {
 }
 
 export default function SearchBy({ type, input, isEnd, placeholder }: IAppProps) {
+
+	const handleSearchClick = (): void => {
+		console.log("search!");
+	}
+
 	return (
-		<StyleSearch>
-			{type}
-			{input && <input placeholder={input}></input>}
-			{placeholder && <div>{placeholder}</div>}
+		<>
+		<StyleSearch isEnd= {isEnd}>
+			<SearchWrapper isEnd={isEnd}>
+				{type}
+				{input && <SearchLocationStyle placeholder={input}></SearchLocationStyle>}
+				{placeholder && <div>{placeholder}</div>}
+			</SearchWrapper>
 		</StyleSearch>
+			{isEnd && <StyleSearchBtn onClick ={handleSearchClick}><SearchIcon stroke="#FFFFFF"/></StyleSearchBtn>}
+		</>
 	);
 }
 
-const StyleSearch = styled.button`
+const StyleSearch = styled.button <{isEnd: boolean}>`
 	font-family: "Noto Sans KR", sans-serif;
-	width: 20em;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+	width: ${(props) => (props.isEnd ? `${40}%` : `${25}%` )};
 	height: 100%;
 	border-radius: 60px;
 	background-color: #ffffff;
@@ -30,3 +38,31 @@ const StyleSearch = styled.button`
 		background-color: #e0e0e0;
 	}
 `;
+
+const SearchWrapper = styled.div < { isEnd: boolean } >`
+	height: 44px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: space-around;
+	border-right: ${(props) => (props.isEnd ? 'none' : '1px solid #E0E0E0')};
+	margin-left: 20px;
+`
+
+const SearchLocationStyle = styled.input`
+	border:none;
+	
+`
+
+const StyleSearchBtn = styled.button`
+	position: absolute;
+	width: 50px;
+	height: 50px;
+	left: 93%;
+	background-color: #E84C60;
+	border-radius: 50%;
+	transition: background-color 0.5s;
+	:hover {
+		background-color : #d12038;
+	}
+`
