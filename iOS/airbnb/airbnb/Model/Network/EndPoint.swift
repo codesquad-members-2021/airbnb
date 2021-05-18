302b832c43b9fc44b6b7034fa7a7aa4ec50ea126
shimyoungmin <dudals6965@gmail.com>
2021-05-18 12:52:52 +0900

    [#5]feat: Add Network & Endpoint
    첫번째 화면 네트워크를 위한 Network 및 urlPath를 나타낼 endpoint 추가

--- iOS/airbnb/airbnb/Model/Network/EndPoint.swift
+++ iOS/airbnb/airbnb/Model/Network/EndPoint.swift
@@ -6,17 +6,24 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol Requestable {
     var baseURL: String { get }
     var path: String { get }
+    var httpMethod: HTTPMethod { get }
     func url() -> URL?
 }
 
 struct MainAPIEndPoint: Requestable {
-    var baseURL: String
-    
+    var baseURL = ""
     var path: String
+    var httpMethod: HTTPMethod
+    
+    init(path: String, httpMethod: HTTPMethod) {
+        self.path = path
+        self.httpMethod = httpMethod
+    }
     
     func url() -> URL? {
         return URL(string: baseURL + path)
--- iOS/airbnb/airbnb/Model/Network/Network.swift
+++ iOS/airbnb/airbnb/Model/Network/Network.swift
@@ -9,7 +9,26 @@ import Foundation
 import Alamofire
 
 class Network {
-    func request(with endPoint: Requestable, method: HTTPMethod) {
+    func request<T: Decodable> (with endPoint: Requestable, completion: @escaping (Result<T,AFError>) -> Void) {
         
+        guard let url = endPoint.url() else {
+            completion(.failure(AFError.createURLRequestFailed(error: NetworkError.url(description: ("Couldn't Create URL")))))
+            return
+        }
+        
+        AF.request(url, method: endPoint.httpMethod)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .failure(let error):
+                    completion(.failure(error))
+                case .success(let data):
+                    completion(.success((data)))
+                }
+            }
     }
+}
+
+enum NetworkError:Error {
+    case url(description: String)
 }
