57c7794fd0cf410581b65250b9ba6497207e9252
1song2 <1song2918@gmail.com>
2021-05-18 16:45:55 +0900

    feat: add hero image

--- ios/AirbnbApp/AirbnbApp/LandingPageViewController.swift
+++ ios/AirbnbApp/AirbnbApp/LandingPageViewController.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class LandingPageViewController: UIViewController {
+    @IBOutlet weak var getTripIdeasButton: UIButton!
     let searchBar = UISearchBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureSearchBar()
+        configureGetTripIdeasButton()
     }
     
     func configureSearchBar() {
         searchBar.placeholder = "어디로 여행가세요?"
         navigationItem.titleView = searchBar
+    }
+    
+    func configureGetTripIdeasButton() {
+        getTripIdeasButton.layer.masksToBounds = true
+        getTripIdeasButton.layer.cornerRadius = 10.0
+        getTripIdeasButton.contentEdgeInsets = UIEdgeInsets(top: 8.0, left: 16.0, bottom: 8.0, right: 16.0)
     }
 }
