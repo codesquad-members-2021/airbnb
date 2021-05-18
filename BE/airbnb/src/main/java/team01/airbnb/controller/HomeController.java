f94d017c9f7876ae18497ff5ac1c56f598ed7156
Shion <ehdrhelr@gmail.com>
2021-05-18 12:31:16 +0900

    [#3] chore : handlebars 사용
    의존성 추가
    uri에 properties 사용

--- BE/airbnb/src/main/java/team01/airbnb/controller/HomeController.java
+++ BE/airbnb/src/main/java/team01/airbnb/controller/HomeController.java
@@ -2,12 +2,24 @@ package team01.airbnb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import team01.airbnb.utils.KakaoLoginUtils;
 
 @Controller
 public class HomeController {
 
+    private final KakaoLoginUtils kakaoLoginUtils;
+
+    public HomeController(KakaoLoginUtils kakaoLoginUtils) {
+        this.kakaoLoginUtils = kakaoLoginUtils;
+    }
+
     @GetMapping("/")
-    public String home() {
-        return "index.html";
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        mav.addObject("kakao-client-id", kakaoLoginUtils.getClientId());
+        mav.addObject("kakao-redirect-uri", kakaoLoginUtils.getRedirectUri());
+        return mav;
     }
 }
