57886365766736128fd1dc5215a82cb7011439c2
95degree <95degree@naver.com>
2021-05-18 16:59:49 +0900

    feat: ✨CityListWrapper 로직 생성(#5)
    CityList를 Wrapper로 감싸주기 위해 CityListWrapper를 생성

--- BE/src/main/java/airbnb/CityListResponse.java
+++
@@ -1,4 +0,0 @@
-package airbnb;
-
-public class CityListResponse {
-}
---
+++ BE/src/main/java/airbnb/CityListWrapper.java
@@ -0,0 +1,10 @@
+package airbnb;
+
+public class CityListWrapper {
+
+    List<City> cities;
+
+    public CityListWrapper(List<City> cities) {
+        this.cities = cities;
+    }
+}
