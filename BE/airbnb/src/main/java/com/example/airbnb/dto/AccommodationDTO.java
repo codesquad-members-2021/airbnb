5df79d20d3b279f824aaa6452cf5fe56efbd0c70
malaheaven <yunhg95@gmail.com>
2021-05-18 12:15:08 +0900

    feat: Dto 멤버 내용 추가

---
+++ BE/airbnb/src/main/java/com/example/airbnb/dto/AccommodationDTO.java
@@ -0,0 +1,14 @@
+package com.example.airbnb.dto;
+
+public class AccommodationDTO {
+
+    LocationDTO location;
+    String title;
+    Integer point;
+    Integer reviewCount;
+    Integer price;
+    String  image;
+
+    // Boolean isLike;
+
+}
---
+++ BE/airbnb/src/main/java/com/example/airbnb/dto/RecommendedDTO.java
@@ -0,0 +1,8 @@
+package com.example.airbnb.dto;
+
+public class RecommendedDTO {
+    String cityName;
+    String description;
+    String image;
+
+}
