09232b612aaf904bac22543e33b0a8c8aaceccae
Jisoo Kim <ghis22130@naver.com>
2021-05-18 16:23:02 +0900

    feat: 검색, 위시리스트 ViewController 생성

---
+++ iOS/Arirbnb/Arirbnb/MyReservationScene/View/MyReserVationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyReserVationViewController.swift
+//  Arirbnb
+//
+//  Created by 지북 on 2021/05/18.
+//
+
+import UIKit
+
+class MyReserVationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ iOS/Arirbnb/Arirbnb/WishListScene/View/WishListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WishListViewController.swift
+//  Arirbnb
+//
+//  Created by 지북 on 2021/05/18.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
