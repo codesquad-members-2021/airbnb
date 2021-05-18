4f3631343c76cc2f2d8a84ffa41fb7857568b355
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-18 11:36:25 +0900

    chore: 프로젝트 기본 세팅
    Dae-Hwa/airbnb/#3

---
+++ BE/airbnb-be/src/main/java/com/codesquad/airbnb/AirbnbBeApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbBeApplication.class, args);
+    }
+
+}
---
+++ BE/airbnb-be/src/main/java/com/codesquad/airbnb/sample/SampleController.java
@@ -0,0 +1,12 @@
+package com.codesquad.airbnb.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
