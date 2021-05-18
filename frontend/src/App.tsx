914c437e57c84734f6deace6689c5f4663f32671
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-18 14:47:58 +0900

    add: 히어로 이미지 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import Header from "./components/main/header/Header";
+import Main from "./components/main/Main";
+import styled from "styled-components";
 
 function App() {
 	return (
 		<div>
-			<Header />
+			<MainWrapper>
+				<Header />
+				<Main />
+			</MainWrapper>
 		</div>
 	);
 }
 
 export default App;
+
+const MainWrapper = styled.div`
+	background: url("/hero.jpg") no-repeat center;
+	background-size: cover;
+	min-width: 2560px;
+	min-height: 1280px;
+`;
