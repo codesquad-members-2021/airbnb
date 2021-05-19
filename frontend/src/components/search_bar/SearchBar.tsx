393ad349e0bb424552940529f173738900feb929
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-19 22:17:21 +0900

    chore: 인덴트 동기화

--- frontend/src/components/search_bar/SearchBar.tsx
+++ frontend/src/components/search_bar/SearchBar.tsx
@@ -7,11 +7,11 @@ export default function SearchBar(props: IAppProps) {
 	return (
 		<StyleSearchBar>
 			{/* 검색 조건에 지역 넣게되면 아래 줄 주석 풀기 */}
-			{/* <SearchFilter type="LOCATION" input="어디로 여행가세요?" isEnd={false} /> */} 
+			{/* <SearchFilter type="LOCATION" input="어디로 여행가세요?" isEnd={false} /> */}
 			<SearchFilter type="CHECKIN" isEnd={false} placeholder={true} />
-			<SearchFilter type="CHECKOUT" isEnd={false} placeholder= {true} />
-			<SearchFilter type="FEE" isEnd={false} placeholder= {true} />
-			<SearchFilter type="GUEST" isEnd={true} placeholder= {true} />
+			<SearchFilter type="CHECKOUT" isEnd={false} placeholder={true} />
+			<SearchFilter type="FEE" isEnd={false} placeholder={true} />
+			<SearchFilter type="GUEST" isEnd={true} placeholder={true} />
 		</StyleSearchBar>
 	);
 }
@@ -24,7 +24,7 @@ const StyleSearchBar = styled.div`
 	width: 916px;
 	height: 76px;
 	top: 110px;
-	left: 270px;
+	left: 25%;
 	border-radius: 60px;
 	background-color: #ffffff;
 `;
