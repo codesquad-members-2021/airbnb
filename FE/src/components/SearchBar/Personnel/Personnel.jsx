73c8cd9d47e2a3a2fa7c7183b8dfaadad4c057a6
Seong Young Jeong <tjdduddl10@gmail.com>
2021-05-19 18:12:18 +0900

    Feat: [#29] Personnel 컴포넌트 구현

--- FE/src/components/SearchBar/Personnel/Personnel.jsx
+++ FE/src/components/SearchBar/Personnel/Personnel.jsx
@@ -1,13 +1,44 @@
 import styled from "styled-components";
-// import CloseButton from "../CloseButton";
+import PersonnelModal from "./PersonnelModal";
+import CloseButton from "../CloseButton";
+import { useEffect, useRef, useState } from "react";
 
-const Personnel = () => (
-	<PersonnelWrapper>
-		<PersonnelContent>인원</PersonnelContent>
-		<PersonnelInput />
-		{/* <CloseButton /> */}
-	</PersonnelWrapper>
-);
+const Personnel = () => {
+	const [isOn, setOn] = useState(false);
+
+	const [man, setMan] = useState(0);
+	const [kid, setKid] = useState(0);
+	const [baby, setBaby] = useState(0);
+
+	const isActivated = Boolean(man || kid || baby);
+
+	const currentDOM = useRef();
+
+	useEffect(() => {
+		document.addEventListener("click", (e) => {
+			if (currentDOM.current && !currentDOM.current.contains(e.target)) setOn(() => false);
+		});
+	}, []);
+
+	useEffect(() => {
+		if (man === 0 && (kid || baby)) setMan(() => 1);
+	}, [man, kid, baby]);
+
+	const resetEvent = () => {
+		setMan(() => 0);
+		setKid(() => 0);
+		setBaby(() => 0);
+	};
+
+	return (
+		<PersonnelWrapper ref={currentDOM} onClick={() => setOn(() => true)}>
+			<PersonnelContent>인원</PersonnelContent>
+			<PersonnelInput value={isActivated ? `게스트 ${man + kid}명, 유아${baby}명` : ""} readOnly />
+			{isOn && <PersonnelModal people={{ man, setMan, kid, setKid, baby, setBaby }} />}
+			{isActivated && <CloseButton onClick={resetEvent} />}
+		</PersonnelWrapper>
+	);
+};
 
 const PersonnelWrapper = styled.div`
 	position: relative;
---
+++ FE/src/components/SearchBar/Personnel/PersonnelModal.jsx
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+const PersonnelModal = ({ people }) => {
+	const { man, setMan, kid, setKid, baby, setBaby } = people;
+	return (
+		<PersonnelModalWrapper>
+			<Content title="성인" body="만 13세 이상" count={man} setCount={setMan} />
+			<Line />
+			<Content title="어린이" body="만 2세~12세" count={kid} setCount={setKid} />
+			<Line />
+			<Content title="유아" body="만 2세 미만" count={baby} setCount={setBaby} />
+		</PersonnelModalWrapper>
+	);
+};
+
+const Content = ({ title, body, count, setCount }) => (
+	<ContentWrapper>
+		<ContentTitle>{title}</ContentTitle>
+		<ContentBody>{body}</ContentBody>
+		<Controller>
+			<MinusButton disable={count < 1} onClick={() => setCount((count) => (count < 1 ? 0 : count - 1))} />
+			<Count>{count}</Count>
+			<PlusButton disable={count > 7} onClick={() => setCount((count) => (count > 7 ? 8 : count + 1))} />
+		</Controller>
+	</ContentWrapper>
+);
+
+const MinusButton = ({ disable, onClick }) => (
+	<svg onClick={onClick} width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M18 33C26.2843 33 33 26.2843 33 18C33 9.71573 26.2843 3 18 3C9.71573 3 3 9.71573 3 18C3 26.2843 9.71573 33 18 33Z"
+			stroke={disable ? "#E0E0E0" : "#828282"}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path d="M12 18H24" stroke={disable ? "#E0E0E0" : "#828282"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+	</svg>
+);
+
+const PlusButton = ({ disable, onClick }) => (
+	<svg onClick={onClick} width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M18 33C26.2843 33 33 26.2843 33 18C33 9.71573 26.2843 3 18 3C9.71573 3 3 9.71573 3 18C3 26.2843 9.71573 33 18 33Z"
+			stroke={disable ? "#E0E0E0" : "#828282"}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path d="M18 12V24" stroke={disable ? "#E0E0E0" : "#828282"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M12 18H24" stroke={disable ? "#E0E0E0" : "#828282"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+	</svg>
+);
+
+const PersonnelModalWrapper = styled.div`
+	position: absolute;
+	width: 400px;
+	height: 355px;
+	left: -80px;
+	top: 76px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background: #ffffff;
+	box-shadow: 0px 4px 10px rgba(51, 51, 51, 0.1), 0px 0px 4px rgba(51, 51, 51, 0.05);
+	border-radius: 40px;
+`;
+const ContentWrapper = styled.div`
+	position: relative;
+	width: 272px;
+	height: 43px;
+`;
+const ContentTitle = styled.div`
+	font-weight: bold;
+	font-size: 16px;
+	line-height: 23px;
+	color: #010101;
+`;
+const ContentBody = styled.div`
+	font-size: 14px;
+	line-height: 20px;
+	color: #828282;
+`;
+const Controller = styled.div`
+	position: absolute;
+	top: 4px;
+	left: 154px;
+	width: 116px;
+	height: 36px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+const Count = styled.div`
+	font-weight: bold;
+	font-size: 20px;
+	line-height: 29px;
+	color: #333333;
+`;
+const Line = styled.div`
+	width: 272px;
+	height: 1px;
+	background: #c4c4c4;
+	margin: 24px;
+`;
+
+export default PersonnelModal;
