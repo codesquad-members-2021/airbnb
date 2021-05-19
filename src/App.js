1ccf8cf72d1aacc53f9f22c64e453d795ffe6471
jjunyjjuny <jjuny546@gmail.com>
2021-05-20 02:25:37 +0900

    style : markup /search

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import SearchPage from "./components/search/SearchPage";
 import GlobalStyle from "./utils/GlobalStyles";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header></Header>
       <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/search" component={SearchPage} />
+        {/* <Route path="/" component={HomePage} exact /> */}
+        <Route path="/" component={SearchPage} exact />
+        {/* <Route path="/search" component={SearchPage} /> */}
       </Switch>
     </>
   );
