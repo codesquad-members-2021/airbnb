9bb7ccdcf76d907f24cf11a9293d8c6753fb4f2e
Sonjh1306 <yale1306@gmail.com>
2021-05-19 15:47:39 +0900

    [#3] style: 코드 부분 수정

--- ios/swift-airbnb/swift-airbnb/MainViewController.swift
+++ ios/swift-airbnb/swift-airbnb/MainViewController.swift
@@ -17,16 +17,8 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        network.reequest(url: Url.base.rawValue, completionHandler: { (result : Result<TestDTO, Error>) in
-//            switch result {
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            case .success(let data):
-//                print(data)
-//            }
-//        })
         configureDataSource()
-  
+
     }
     
     func configureDataSource() {
