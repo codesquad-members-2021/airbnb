caeb0b77509016c0d0ac631686e4ba066419438e
dong <ultraraptor@naver.com>
2021-05-19 12:31:37 +0900

    메인페이지에 팝업 해 줄 정보들을 백엔드에서 보내주도록 로직과 데이터 추가

--- BE/airbnb/src/main/java/com/example/airbnb/dto/ThemeDTO.java
+++ BE/airbnb/src/main/java/com/example/airbnb/dto/ThemeDTO.java
@@ -4,4 +4,32 @@ public class ThemeDTO {
     String title;
     String image;
 
+    public ThemeDTO(String title, String image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "ThemeDTO{" +
+                "title='" + title + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
 }
--- BE/airbnb/src/main/java/com/example/airbnb/service/SuggestionsService.java
+++ BE/airbnb/src/main/java/com/example/airbnb/service/SuggestionsService.java
@@ -10,12 +10,27 @@ import java.util.List;
 
 @Service
 public class SuggestionsService {
+    private static List<RecommendedDTO> recommendedDTOList = new ArrayList<>();
+    private static List<ThemeDTO> themeDTOList = new ArrayList<>();
+
+    static {
+        recommendedDTOList.add(new RecommendedDTO("서울","차로 30분 거리",""));
+        recommendedDTOList.add(new RecommendedDTO("인천","차로 1시간 거리",""));
+        recommendedDTOList.add(new RecommendedDTO("의정부시","차로 1시간 거리",""));
+        recommendedDTOList.add(new RecommendedDTO("대구","차로 4시간 거리",""));
+        recommendedDTOList.add(new RecommendedDTO("대전","차로 2시간 거리",""));
+        recommendedDTOList.add(new RecommendedDTO("광주","차로 4시간 거리",""));
+
+
+        themeDTOList.add(new ThemeDTO("자연생활을 만끽할 수 있는 숙소",""));
+        themeDTOList.add(new ThemeDTO("독특한 공간",""));
+        themeDTOList.add(new ThemeDTO("집 전체",""));
+        themeDTOList.add(new ThemeDTO("반려동물 동반 가능",""));
+    }
+
     public static MainPageDTO mainPage() {
 
-        List<RecommendedDTO> recommendedDTOList = new ArrayList<>();
-        List<ThemeDTO> themeDTOList = new ArrayList<>();
-        new ThemeDTO();
-        new RecommendedDTO();
+
 
 
         return new MainPageDTO(recommendedDTOList,themeDTOList);
