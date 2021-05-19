ae3f99393bc58a4b7e10a0791e9e66e1d18ec281
jjunyjjuny <jjuny546@gmail.com>
2021-05-20 02:29:45 +0900

    chore : rebase

---
+++ src/components/home/Thema.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const Thema = ({ contents }) => {
+  return <ThemaWrapper>{contents.map(renderContent)}</ThemaWrapper>;
+};
+const renderContent = (content) => {
+  const { imageURL, themaTitle } = content;
+  return (
+    <ThemaItem URL={imageURL}>
+      <div />
+      <h3>{themaTitle}</h3>
+    </ThemaItem>
+  );
+};
+const ThemaWrapper = styled.ul`
+  display: flex;
+`;
+const ThemaItem = styled.li`
+  flex: 1;
+  & + & {
+    margin-left: 1rem;
+  }
+
+  & div {
+    border-radius: 1rem;
+    width: 100%;
+    background: url(${({ URL }) => URL});
+    background-size: cover;
+  }
+  & div:after {
+    content: "";
+    display: block;
+    padding-bottom: 100%;
+  }
+  & h3 {
+    margin-top: 1rem;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+`;
+
+export default Thema;
---
+++ src/components/search/SearchPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchPage = () => {
+  return <SearchPageWrapper></SearchPageWrapper>;
+};
+
+const SearchPageWrapper = styled.div``;
+
+export default SearchPage;
