d536195f1d950b681c8ea4340f33ce87a23f28ee
95degree <95degree@naver.com>
2021-05-20 17:03:54 +0900

    feat: ✨ CitiesWrapper 생성자 및 getter 추가 (#5)

--- BE/src/main/java/airbnb/wrapper/CitiesWrapper.java
+++ BE/src/main/java/airbnb/wrapper/CitiesWrapper.java
@@ -1,4 +1,18 @@
 package airbnb.wrapper;
 
+import airbnb.domain.City;
+
+import java.util.List;
+
 public class CitiesWrapper {
+
+    private List<City> cities;
+
+    public CitiesWrapper(List<City> cities) {
+        this.cities = cities;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
 }
