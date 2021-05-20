2ee1947cd27a3a6a6ae8924c707444102ed8a7c4
JinseoPark <rarebook92@naver.com>
2021-05-20 17:02:21 +0900

    feat :sparkles: set the button to searchView

--- iOS/JacksonBnB/JacksonBnB/View/LottieAnimation.swift
+++ iOS/JacksonBnB/JacksonBnB/View/LottieAnimation.swift
@@ -14,7 +14,7 @@ class LottieAnimation {
     
     init(with view: UIView) {
         animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-        animationView.center = view.center
+//        animationView.center = view.center
         animationView.contentMode = .scaleAspectFill
     }
     
