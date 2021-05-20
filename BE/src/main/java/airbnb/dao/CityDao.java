199b90f5b2badf0fc7551976931e7cff7181f5ee
95degree <95degree@naver.com>
2021-05-20 12:01:06 +0900

    feat: ✨ findAll 메소드 생성(#5)
    모든 도시의 값을 가져오기 위해 findAll 메소드를 생성

--- BE/src/main/java/airbnb/dao/CityDao.java
+++ BE/src/main/java/airbnb/dao/CityDao.java
@@ -1,4 +1,24 @@
 package airbnb.dao;
 
+import airbnb.domain.City;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
 public class CityDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private CityMapper cityMapper = new CityMapepr;
+
+    public CityDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<City> findAll(){
+        String sql = "SELECT id, name FROM city";
+        return jdbcTemplate.query(sql,CityMapper);
+    }
 }
