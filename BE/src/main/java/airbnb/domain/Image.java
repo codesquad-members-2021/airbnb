13964f1539d643e50d45ae7553e3ae5bde095fb2
95degree <95degree@naver.com>
2021-05-20 17:23:54 +0900

    feat: ✨Image에 필드 추가(#5)
    - id, url, imageTypeId, ImageClassId를 필드로 추가

--- BE/src/main/java/airbnb/domain/Image.java
+++ BE/src/main/java/airbnb/domain/Image.java
@@ -1,4 +1,42 @@
 package airbnb.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Embedded;
+
 public class Image {
+
+    @Id
+    private Long id;
+    private String url;
+
+    @Column(value = "image_type_id")
+    private Long imageTypeId;
+
+    @Embedded.Nullable
+    private ImageClassId imageClassId;
+
+
+    public Image(Long id, String url, Long imageTypeId, ImageClassId imageClassId) {
+        this.id = id;
+        this.url = url;
+        this.imageTypeId = imageTypeId;
+        this.imageClassId = imageClassId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Long getImageTypeId() {
+        return imageTypeId;
+    }
+
+    public ImageClassId getImageClassId() {
+        return imageClassId;
+    }
 }
