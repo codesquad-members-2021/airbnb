e156152626d34ef17f27137a45f3a555927ec705
95degree <95degree@naver.com>
2021-05-20 12:14:25 +0900

    feat: ✨ CityMapper 생성(#5)

---
+++ BE/src/main/java/airbnb/mapper/CityMapper.java
@@ -0,0 +1,15 @@
+package airbnb.mapper;
+
+import airbnb.domain.City;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class CityMapper implements RowMapper<City> {
+    @Override
+    public City mapRow(ResultSet rs, int rowNum) throws SQLException{
+        return new City(rs.getLong("id"),rs.getString("name"));
+    }
+}
