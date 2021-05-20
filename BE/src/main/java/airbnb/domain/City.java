bf857120ab65511e756f6b9c601990c4918b68f0
95degree <95degree@naver.com>
2021-05-20 12:15:53 +0900

    feat: ✨ City domain의 생성자 및 getter 추가 (#5)

--- BE/src/main/java/airbnb/domain/City.java
+++ BE/src/main/java/airbnb/domain/City.java
@@ -6,4 +6,17 @@ public class City {
     @Id
     private Long id;
     private String name;
+
+    public City(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
