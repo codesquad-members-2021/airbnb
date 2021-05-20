5a5a60ce8f22081ac444415bee89ed6a4d19388e
zeke <yangsin1031@naver.com>
2021-05-20 12:02:08 +0900

    Feat: SearchController UI구현

---
+++ ios/airbnb/airbnb/CityCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CityCollectionViewCell.swift
+//  airbnb
+//
+//  Created by 양준혁 on 2021/05/20.
+//
+
+import UIKit
+
+class CityCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ios/airbnb/airbnb/Controller/LocationViewController.swift
+++ ios/airbnb/airbnb/Controller/LocationViewController.swift
@@ -7,14 +7,30 @@
 
 import UIKit
 
+struct City {
+    let name: String
+}
+
+struct DetailCity {
+    let name: String
+}
+
 final class LocationViewController: UIViewController {
     
+    @IBOutlet weak var cityCollectionView: UICollectionView!
+    
     private let searchController = UISearchController(searchResultsController: nil)
+    var cities = [City(name: "seoul"), City(name: "busan")]
+    var detailCities = [DetailCity(name: "songpa"), DetailCity(name: "jamsil")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItem()
-        configureSearchBar()
+        configureSearchController()
+        registerNib()
+        cityCollectionView.dataSource = self
+        searchController.searchResultsUpdater = self
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,19 +38,65 @@ final class LocationViewController: UIViewController {
     }
     
     private func configureNavigationItem() {
-        navigationController?.navigationItem.backBarButtonItem?.title = "뒤로"
         self.navigationItem.title = "숙소 찾기"
         let cancel = UIBarButtonItem(title: "지우기", style: .done, target: self, action: #selector(tappedDeleteBarButton))
         self.navigationItem.rightBarButtonItem = cancel
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+
     }
     
-    private func configureSearchBar() {
+    private func configureSearchController() {
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.automaticallyShowsCancelButton = false
+        searchController.searchBar.placeholder = "어디로 여행가세요?"
         self.navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+    
+    func registerNib() {
+        let nib = UINib(nibName: "CityCollectionViewCell", bundle: nil)
+        cityCollectionView.register(nib, forCellWithReuseIdentifier: "cell")
     }
     
     @objc func tappedDeleteBarButton() {
         
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+}
+
+extension LocationViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if isFiltering() {
+            return cities.count
+        } else {
+            return detailCities.count
+        }
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CityCollectionViewCell
+        
+        if isFiltering() {
+            cell.label.text = detailCities[indexPath.row].name
+        } else {
+            cell.label.text = cities[indexPath.row].name
+        }
+        return cell
+    }
+    
+    
+}
+
+extension LocationViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        self.cityCollectionView.reloadData()
     }
 }
