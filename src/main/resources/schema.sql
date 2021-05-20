f4759aa85acb3c5032dc57b5a96589df3ca8cdae
bibi <bibi6666667@kakao.com>
2021-05-20 18:29:44 +0900

    add : schema.sql 생성 #5

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,78 +1,86 @@
-drop table if exists room;
-drop table if exists booking;
+drop table if exists `badges`;
+drop table if exists `thumbnails`;
 drop table if exists `option`;
+drop table if exists `wish`;
+drop table if exists `booking`;
 drop table if exists `user`;
-drop table if exists wish;
-drop table if exists badges;
-drop table if exists thumbnails;
+drop table if exists `room`;
 
-create table room (
-    id int not_null auto_increment,
-    max int not_null,
-    name varchar(45) not_null,
-    rating double not_null,
-    latitude double not_null,
-    longitude double not_null,
-    bedroom_count int not_null,
-    bed_count int not_null,
-    bathroom_count int not_null,
-    address varchar(45) not_null,
-    detail_address varchar(45) not_null,
-    comment_count int not_null,
-    original_price int not_null,
-    sale_price int not_null,
-    flexible_refund tinyint(1) not_null,
-    immediate_booking tinyint(1) not_null,
+create table `room`
+(
+    `id`                int not null auto_increment,
+    `max`               int not null,
+    `name`              varchar(100) not null,
+    `rating`            double not null,
+    `latitude`          double not null,
+    `longitude`         double not null,
+    `bedroom_count`     int not null,
+    `bed_count`         int not null,
+    `bathroom_count`    int not null,
+    `address`           varchar(45) not null,
+    `detail_address`    varchar(45) not null,
+    `comment_count`     int not null,
+    `original_price`    int not null,
+    `sale_price`        int not null,
+    `flexible_refund`   tinyint(1) not null,
+    `immediate_booking` tinyint(1) not null,
     primary key (id)
 );
 
-create table user (
-    id int not_null auto_increment,
-    user_id varchar(45) not_null,
-    password varchar(45) not_null,
+create table `user`
+(
+    id        int not null auto_increment,
+    github_id varchar(45) not null,
     primary key (id)
 );
 
-create table booking (
-    id int not_null auto_increment,
-    user_id int not_null,
-    room_id int not_null,
-    check_in datetime not_null,
-    check_out datetime not_null,
-    adult int not_null,
-    child int not_null,
-    baby int not_null,
-    total int not_null,
+create table `booking`
+(
+    id        int not null auto_increment,
+    room_id   int not null,
+    user_id   int not null,
+    check_in  datetime not null,
+    check_out datetime not null,
+    adult     int not null,
+    child     int not null,
+    baby      int not null,
+    total     int not null,
     primary key (id),
     foreign key (user_id) references user (id),
     foreign key (room_id) references room (id)
 );
 
-create table wish (
-    id int not_null auto_increment,
-    room_id int not_null,
-    user_id int not_null,
+create table `wish`
+(
+    id      int not null auto_increment,
+    room_id int not null,
+    user_id int not null,
     primary key (id),
     foreign key (user_id) references user (id),
     foreign key (room_id) references room (id)
 );
 
-create table option (
-    room_id int not_null,
-    kitchen tinyint(1) not_null,
-    air_conditioner tinyint(1) not_null,
-    wifi tinyint(1) not_null,
-    free_parking_lot tinyint(1) not_null,
-    hair_dryer tinyint(1) not_null,
+create table `option`
+(
+    room_id          int not null,
+    kitchen          tinyint(1) not null,
+    air_conditioner  tinyint(1) not null,
+    wifi             tinyint(1) not null,
+    free_parking_lot tinyint(1) not null,
+    hair_dryer       tinyint(1) not null,
     foreign key (room_id) references room (id)
 );
 
-create table thumbnails (
-    thumbnail varchar(64) not_null,
+create table `thumbnails`
+(
+    room_id   int not null,
+    thumbnail varchar(200) not null,
     foreign key (room_id) references room (id)
 );
 
-create table badges (
-    type varchar(64) not_null,
+create table `badges`
+(
+    room_id int not null,
+    type    varchar(64) not null,
     foreign key (room_id) references room (id)
 );
