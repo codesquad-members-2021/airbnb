0c8edb4ed834b31f0c07854c8b613d815f5c7669
DongHunKim <ultraraptor@naver.com>
2021-05-21 15:43:19 +0900

    데이터 7곳의 숙소 정보가 리턴되도록 다섯개의 데이터 셋 추가

--- BE/airbnb/src/main/resources/data.sql
+++ BE/airbnb/src/main/resources/data.sql
@@ -6,4 +6,11 @@ use airbnb;
 INSERT INTO accommodation(title, reviewRating, reviewCount, charge, badge, image)
 values
 ('더 스위트 캐슬, 달콤한 휴식',4.92,55,75000,'',''),
-('교보문고 인접거리, 강남대로 중심 최고의 전망',4.25,165,96000,'','');
+('교보문고 인접거리, 강남대로 중심 최고의 전망',4.25,165,96000,'',''),
+('스튜디오 오아시스 스위트(Studio Oasis Suite) 양재역 5분 거리',0,0,90190,'슈퍼호스트',''),
+('Private Comfy Studio - 2 Queen Beds',4.78,82,85220,'',''),
+('Kang-Nam_Cozy_Clean_Quiet',4.72, 82,66800,'',''),
+('NEW)강남역1분! 알콜소독! 영화&커피 Healing House/Movie&Coffee',5.0,22,79167,'청결강화',''),
+('강남,역삼역 15분 넓어요 자가격리불가능Gangnam St.15min wide spaceⅡ',4.67,51,53707,'',''),
+('3 Mins Gangnam STN *Twin Bed* 주차불가*',4.85,34,52567,'','');
+
