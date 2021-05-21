df7410105677954b603a57555747f141e394409f
95degree <95degree@naver.com>
2021-05-21 12:09:15 +0900

    feat: ✨createAllToRequestList 생성(#5)
    - 모든 도시를 List<CityRequest>로 만들어 주기 위해 메소드 생성

--- BE/src/main/java/airbnb/Service/CityService.java
+++ BE/src/main/java/airbnb/Service/CityService.java
@@ -1,7 +1,12 @@
 package airbnb.Service;
 
 import airbnb.dao.CityDao;
+import airbnb.domain.City;
+import airbnb.dto.CityRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CityService {
@@ -10,5 +15,22 @@ public class CityService {
 
     public CityService(CityDao cityDao) {
         this.cityDao = cityDao;
+    }
+
+    public List<City> findAll() {
+        return cityDao.findAll();
+    }
+
+    public List<CityRequest> createAllToRequestList() {
+
+        List<CityRequest> requestList = new ArrayList<>();
+        List<City> cities = findAll();
+
+        for (City city : cities) {
+            CityRequest cityRequest = CityRequest.of(city, city.findMainImageUrl());
+            requestList.add(cityRequest);
+        }
+
+        return requestList;
     }
 }
