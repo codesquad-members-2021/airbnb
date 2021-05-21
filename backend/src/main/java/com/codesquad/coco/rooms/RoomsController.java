f1a2697750f3c954665e2ab4ec4a52f1c4636a77
gisungchoi <chlrltjd159@naver.com>
2021-05-21 14:10:27 +0900

    feat : controller 정의
    - 기본 기능 정의

---
+++ backend/src/main/java/com/codesquad/coco/rooms/RoomsController.java
@@ -0,0 +1,27 @@
+package com.codesquad.coco.rooms;
+
+import com.codesquad.coco.rooms.model.dto.SearchInfoDTO;
+import com.codesquad.coco.rooms.model.dto.SearchRoomsDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomsController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/search/price")
+    public void searchPrice(SearchInfoDTO searchInfoDTO){
+        logger.debug(searchInfoDTO.toString());
+        //todo : 들어온 것으로 price 검색
+    }
+
+    @GetMapping("/search")
+    public void searchRooms(SearchRoomsDTO roomsDTO){
+        logger.debug(roomsDTO.toString());
+        //todo : 들어온 정보로 예약 가능한 곳 검색
+        // maxGuestt와 price의 min max , check 날짜
+    }
+}
