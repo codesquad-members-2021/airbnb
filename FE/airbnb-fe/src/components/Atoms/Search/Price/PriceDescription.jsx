35de2f89d59bbe8776c673e5eca9a587bfe75738
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-05-21 18:24:10 +0900

    feat: Price UI 구성
    - Price modal UI 구성
    - d-h-k/airbnb/#31

--- FE/airbnb-fe/src/components/Atoms/Search/Price/PriceDescription.jsx
+++ FE/airbnb-fe/src/components/Atoms/Search/Price/PriceDescription.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const PriceDescription = () => {
-  return <div></div>;
+  return (
+    <PriceDescriptionDiv>
+      <PriceTitle>가격 범위</PriceTitle>
+      <PriceCaption>
+        <PriceCaptionRange>₩100,000 - ₩1,000,000+</PriceCaptionRange>
+        <PriceCaptionSpan>평균 1박 요금은 ₩165,556 입니다.</PriceCaptionSpan>
+      </PriceCaption>
+    </PriceDescriptionDiv>
+  );
 };
 
+const PriceDescriptionDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+`;
+const PriceTitle = styled.div`
+  font-weight: 700;
+  font-size: ${({ theme }) => theme.fontSizes.M};
+`;
+const PriceCaption = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const PriceCaptionRange = styled.div`
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+const PriceCaptionSpan = styled.div`
+  color: ${({ theme }) => theme.colors.gray3};
+  font-size: ${({ theme }) => theme.fontSizes.XXS};
+`;
 export default PriceDescription;
