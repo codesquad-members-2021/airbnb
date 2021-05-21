074c7693b88017cfce295aa202d86f3934524594
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-22 08:13:15 +0900

    refactor: authorize 메서드에서 불필요한 파라미터 삭제

--- backend/server/src/main/java/com/airbnb/intercepter/LoginInterceptor.java
+++ backend/server/src/main/java/com/airbnb/intercepter/LoginInterceptor.java
@@ -4,8 +4,6 @@ import com.airbnb.annotation.LoginRequired;
 import com.airbnb.dto.UserDto;
 import com.airbnb.exception.AuthenticationException;
 import com.airbnb.util.JwtUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,30 +15,21 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Configuration
 public class LoginInterceptor implements HandlerInterceptor {
-    private final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String authorization = request.getHeader(AUTHORIZATION);
         if (((HandlerMethod) handler).hasMethodAnnotation(LoginRequired.class)) {
-            authenticate(authorization, request);
+            authenticate(request);
         }
         return true;
     }
 
-    private void authenticate(String authorization, HttpServletRequest request) {
-        String[] splitAuth = authorization.split(" ");
+    private void authenticate(HttpServletRequest request) {
+        String[] splitAuth = request.getHeader(AUTHORIZATION).split(" ");
         String tokenType = splitAuth[0].toLowerCase();
         if (splitAuth.length < 1 || !tokenType.equals("bearer")) {
             throw new AuthenticationException("잘못된 Authorization Header 입니다.");
         }
-        String token = splitAuth[1];
-        handleJwt(token, request);
-    }
-
-    private void handleJwt(String token, HttpServletRequest request) {
-        UserDto userDto = JwtUtil.decodeJwt(token);
-        logger.info("login:{}, name: {}", userDto.getLogin(), userDto.getName());
+        UserDto userDto = JwtUtil.decodeJwt(splitAuth[1]);
         request.setAttribute("user", userDto);
     }
 }
