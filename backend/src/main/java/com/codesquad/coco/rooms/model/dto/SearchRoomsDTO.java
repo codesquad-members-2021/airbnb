21bc6b68be3b7f1d32f9f0a10df5483f901ea581
gisungchoi <chlrltjd159@naver.com>
2021-05-21 12:22:37 +0900

    feat : 검색 사항을 저장할 DTO 정의
    - DTO를 정의
    - 검색 조건이 없으면 기본값으로 세팅

--- backend/src/main/java/com/codesquad/coco/rooms/model/dto/SearchRoomsDTO.java
+++ backend/src/main/java/com/codesquad/coco/rooms/model/dto/SearchRoomsDTO.java
@@ -1,25 +1,33 @@
 package com.codesquad.coco.rooms.model.dto;
 
-
-import org.springframework.format.annotation.DateTimeFormat;
+import com.codesquad.coco.rooms.model.Guest;
+import com.codesquad.coco.rooms.model.Money;
 
 import java.beans.ConstructorProperties;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class SearchInfoDTO {
+public class SearchRoomsDTO {
 
     private static String DEFAULT_CITY_NAME = "서울";
 
     private LocalDate checkIn;
     private LocalDate checkOut;
     private String cityName;
+    private Guest guest;
+    private Money priceMin;
+    private Money priceMax;
 
-    @ConstructorProperties({"check-in","check-out","city-name"})
-    public SearchInfoDTO(String checkIn, String checkOut, String cityName) {
+    @ConstructorProperties({"check-in","check-out","city-name","price-min","price-max","adult","child","baby"})
+    public SearchRoomsDTO(String checkIn, String checkOut, String cityName,
+                          int adult,int child,int baby,
+                          int priceMin, int priceMax) {
         this.checkIn = checkIn != null ? LocalDate.parse(checkIn, DateTimeFormatter.ISO_DATE) : LocalDate.of(1993,6,11);
         this.checkOut = checkOut != null ? LocalDate.parse(checkOut, DateTimeFormatter.ISO_DATE) : LocalDate.of(2021,10,11);
         this.cityName = cityName != null ? cityName : DEFAULT_CITY_NAME;
+        this.guest = new Guest(adult,child,baby);
+        this.priceMin = new Money(priceMin);
+        this.priceMax = new Money(priceMax);
     }
 
     public LocalDate getCheckIn() {
@@ -34,12 +42,27 @@ public class SearchInfoDTO {
         return cityName;
     }
 
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public int getPriceMin() {
+        return priceMin.getMoney();
+    }
+
+    public int getPriceMax() {
+        return priceMax.getMoney();
+    }
+
     @Override
     public String toString() {
-        return "SearchInfoDTO{" +
+        return "SearchRoomsDTO{" +
                 "checkIn=" + checkIn +
                 ", checkOut=" + checkOut +
                 ", cityName='" + cityName + '\'' +
+                ", guest=" + guest +
+                ", priceMin=" + priceMin +
+                ", priceMax=" + priceMax +
                 '}';
     }
 }
