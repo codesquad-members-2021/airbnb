a6d9edea2f2199984b8f1e6f768545c9455a57ca
95degree <95degree@naver.com>
2021-05-21 14:36:37 +0900

    feat: ✨Image 빌드 패턴 추가(#5)

--- BE/src/main/java/airbnb/domain/Image.java
+++ BE/src/main/java/airbnb/domain/Image.java
@@ -11,11 +11,59 @@ public class Image {
 
     @Column(value = "image_type")
     private ImageType imageType;
+    @Column(value = "city_id")
+    private Long cityId;
+    @Column(value = "room_id")
+    private Long roomId;
+    @Column(value = "category_id")
+    private Long categoryId;
 
-    public Image(Long id, String url, ImageType imageType) {
-        this.id = id;
-        this.url = url;
-        this.imageType = imageType;
+    private Image(Builder builder) {
+        this.id = builder.id;
+        this.url = builder.url;
+        this.imageType = builder.imageType;
+        this.cityId = builder.cityId;
+        this.roomId = builder.roomId;
+        this.categoryId = builder.categoryId;
+    }
+
+    public static class Builder{
+        private final Long id;
+        private final String url;
+
+        private ImageType imageType;
+        private Long cityId;
+        private Long roomId;
+        private Long categoryId;
+
+        public Builder(Long id, String url) {
+            this.id = id;
+            this.url = url;
+        }
+
+        public Builder imageType(ImageType imageType){
+            this.imageType = imageType;
+            return this;
+        }
+
+        public Builder cityId(Long cityId){
+            this.cityId = cityId;
+            return this;
+        }
+
+        public Builder roomId(Long roomId){
+            this.roomId = roomId;
+            return this;
+        }
+
+        public Builder categoryId(Long categoryId){
+            this.categoryId = categoryId;
+            return this;
+        }
+
+        public Builder build(){
+            return new Image(this);
+        }
     }
 
     public Long getId() {
