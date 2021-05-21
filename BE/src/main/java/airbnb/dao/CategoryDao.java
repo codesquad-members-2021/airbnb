0bcb94eccdba24d154c5446735c8df376d5ffe4b
95degree <95degree@naver.com>
2021-05-21 16:04:13 +0900

    feat: ✨CategoryDao 생성(#5)

--- BE/src/main/java/airbnb/dao/CategoryDao.java
+++ BE/src/main/java/airbnb/dao/CategoryDao.java
@@ -1,4 +1,30 @@
 package airbnb.dao;
 
+import airbnb.domain.Category;
+import airbnb.domain.City;
+import airbnb.mapper.CityMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CategoryDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final ImageDao imageDao;
+    private final CategoryMapper categoryMapper = new CityMapper();
+
+
+    public CategoryDao(JdbcTemplate jdbcTemplate, ImageDao imageDao) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.imageDao = imageDao;
+    }
+
+    public List<Category> findAll() {
+        String sql = "SELECT id, name FROM category";
+        List<Category> categories = jdbcTemplate.query(sql, categoryMapper);
+        categories.forEach(category -> category.setImages(imageDao.findByCategoryId(category.getId())));
+        return categories;
+    }
 }
