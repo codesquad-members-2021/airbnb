68ed99c4a89a57260911ee794135a774809b651a
JinseoPark <rarebook92@naver.com>
2021-05-21 14:47:31 +0900

    feat :sparkles: add data in LocationDB when launch App.

--- iOS/JacksonBnB/JacksonBnB/AppDelegate.swift
+++ iOS/JacksonBnB/JacksonBnB/AppDelegate.swift
@@ -11,6 +11,23 @@ import RealmSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-//    print(Realm.Configuration.defaultConfiguration.fileURL!)
+    let locationIDs = [1,2,3,4,5]
+    let locationNames = ["강동구","강서구","강남구","강북구","마포구"]
+    let locationImageNames = ["CodeSquadHotel","CodeSquadHotel","CodeSquadHotel","CodeSquadHotel","CodeSquadHotel"]
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        
+        
+        let realm = try! Realm()
+        
+        for idx in 0..<5 {
+            let locationDB = LocationDB(id: locationIDs[idx], name: locationNames[idx], imageName: locationImageNames[idx])
+            try! realm.write {
+                realm.add(locationDB)
+            }
+        }
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+    }
+    
 }
-
--- iOS/JacksonBnB/JacksonBnB/Model/LocationDB.swift
+++ iOS/JacksonBnB/JacksonBnB/Model/LocationDB.swift
@@ -10,11 +10,19 @@ import RealmSwift
 
 class LocationDB: Object {
     
-    @objc dynamic var id: Int = 1
+    @objc dynamic var id: Int = 0
     @objc dynamic var name: String = ""
     @objc dynamic var imageName: String = ""
     
     override class func primaryKey() -> String? {
         return "id"
+    }
+    
+    
+    convenience init(id: Int, name: String, imageName: String) {
+        self.init()
+        self.id = id
+        self.name = name
+        self.imageName = imageName
     }
 }
