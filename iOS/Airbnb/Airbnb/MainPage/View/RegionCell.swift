a3ec1ac9b1e3e26384f1000a600a4af2564912ae
Lia <judykim257@gmail.com>
2021-05-21 18:13:18 +0900

    feat: ✨RegionCell 생성(#12)

---
+++ iOS/Airbnb/Airbnb/MainPage/View/RegionCell.swift
@@ -0,0 +1,26 @@
+//
+//  RegionCell.swift
+//  Airbnb
+//
+//  Created by Lia on 2021/05/21.
+//
+
+import UIKit
+
+class RegionCell: UICollectionViewCell {
+
+    static let reuseIdentifier = "RegionCell"
+    static let nib = UINib(nibName: RegionCell.reuseIdentifier, bundle: nil)
+    
+    @IBOutlet weak var regionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func fillUI(with region: CityName) {
+        self.regionLabel.text = region.city
+    }
+
+}
+
