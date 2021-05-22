e371e1f369a21af59d502d47b8f2aa372c3f12c7
cchoongh <cndgh49@naver.com>
2021-05-22 11:11:24 +0900

    fix: 불필요한 코드 삭제 (from 'CalendarSlider/atoms.tsx')

--- fe/airbnb-fe/src/components/ReservationBar/CalendarSlider/atoms.tsx
+++ fe/airbnb-fe/src/components/ReservationBar/CalendarSlider/atoms.tsx
@@ -1,13 +1,4 @@
 import { atom, selector } from 'recoil';
-import { T_CheckInOut, CheckInOut } from '../atoms';
-
-export const IsCompleteCheckInOut = selector<boolean>({
-  key: 'IsCompleteCheckInOut',
-  get: ({ get }) => {
-    const checkInOut: T_CheckInOut = get(CheckInOut);
-    return (checkInOut.in !== null && checkInOut.out !== null);
-  }
-});
 
 export const HoveredDate = atom<number|null>({
   key: 'CurrentHoveredDate',
