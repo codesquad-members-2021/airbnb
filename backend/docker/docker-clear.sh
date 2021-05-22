8040c1f657c3f4b1e886ae41a597350279137118
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-22 11:46:38 +0900

    fix: docker-clear.sh 주석 제거

--- backend/docker/docker-clear.sh
+++ backend/docker/docker-clear.sh
@@ -1,5 +1,5 @@
 docker stop $(docker ps -aq);
 docker rm $(docker ps -aq);
-# docker rmi $(docker images -aq) --force;
+docker rmi $(docker images -aq) --force;
 docker system prune --force;
 docker system prune --volumes --force;
