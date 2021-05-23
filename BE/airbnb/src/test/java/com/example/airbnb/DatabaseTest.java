991a89d60b8f50f122af97090dd0334f272cba77
DHKim <ultraraptor@naver.com>
2021-05-23 12:53:24 +0900

    데이터베이스 테스트코드 추가 및 정리

--- BE/airbnb/src/test/java/com/example/airbnb/DatabaseTest.java
+++ BE/airbnb/src/test/java/com/example/airbnb/DatabaseTest.java
@@ -2,12 +2,16 @@ package com.example.airbnb;
 
 import com.example.airbnb.domain.Accommodation;
 import com.example.airbnb.repository.AccommodationDAO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.Name;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 public class DatabaseTest {
 
@@ -16,16 +20,45 @@ public class DatabaseTest {
 
     private final Logger logger = LoggerFactory.getLogger(DatabaseTest.class);
 
-    @Test
-    void countAll() {
-        int ret = accommodationDAO.countAllAccommodation();
-        logger.info("countAll : {}",ret);
+    @Test()
+    @DisplayName("Create")
+    void insertData() {
+        //System.out.println(accommodationService.insert("샘플숙소3"));
+        //System.out.println(accommodationService.delete("샘플숙소3"));
     }
 
-    @Test
+
+    @Test()
+    @DisplayName("Read")
     void findById() {
         Accommodation accommodation = accommodationDAO.findById(1L);
         logger.info(accommodation.toString());
+        //System.out.println(accommodationService.findById(1L).getTitle());
+
     }
 
+
+
+    @Test()
+    @DisplayName("Count Entity")
+    void countAll() {
+        System.out.println(accommodationDAO.countAllAccommodation());
+        //logger.info("countAll : {}",ret);
+
+    }
+
+    @Test
+    @DisplayName("Update")
+    void update() {
+        //int ret = accommodationDAO.
+    }
+
+    @Test()
+    @DisplayName("Delete")
+    void deleteData() {
+        //System.out.println(accommodationService.insert("샘플숙소3"));
+        //System.out.println(accommodationService.delete("샘플숙소3"));
+    }
+
+    // @todo 모든 DAO 함수에 대하여 유효성 검증하기
 }
