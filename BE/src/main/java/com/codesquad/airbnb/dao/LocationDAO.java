88d3c965293be5e76f28468844dbdec33b42ff05
Sally Oh <min27604@hotmail.com>
2021-05-24 17:51:01 +0900

    refactor: 중복코드 제거

--- BE/src/main/java/com/codesquad/airbnb/dao/LocationDAO.java
+++ BE/src/main/java/com/codesquad/airbnb/dao/LocationDAO.java
@@ -21,11 +21,12 @@ public class LocationDAO {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public class LocationMapper implements RowMapper<Location> {
+    private static class LocationMapper implements RowMapper<Location> {
         public Location mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
             Location location = new Location(resultSet.getLong("id"),
                     resultSet.getString("name"),
                     resultSet.getString("image_url"));
+
             return location;
         }
     }
@@ -33,16 +34,11 @@ public class LocationDAO {
     public Location findById(Long id) {
         String sql = "Select id, name, image_url FROM location WHERE id = ?";
 
-        RowMapper<Location> locationMapper = (rs, rowNum) -> {
-            return new Location(rs.getLong("id"), rs.getString("name"), rs.getString("image_url"));
-        };
-
-        return jdbcTemplate.queryForObject(sql, locationMapper, id);
+        return jdbcTemplate.queryForObject(sql, new LocationMapper(), id);
     }
 
     public List<Location> findAll() {
         String sql = "SELECT id, name, image_url FROM location";
-        List<Location> locations = jdbcTemplate.query(sql, new LocationMapper());
-        return locations;
+        return jdbcTemplate.query(sql, new LocationMapper());
     }
 }
