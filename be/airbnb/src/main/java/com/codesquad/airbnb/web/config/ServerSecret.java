41667f45780cc367506e939032308fb2ac787125
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-24 14:08:46 +0900

    feat: 설정파일에 롬복 적용

--- be/airbnb/src/main/java/com/codesquad/airbnb/web/config/ServerSecret.java
+++ be/airbnb/src/main/java/com/codesquad/airbnb/web/config/ServerSecret.java
@@ -1,5 +1,7 @@
 package com.codesquad.airbnb.web.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -7,23 +9,9 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource(value = "classpath:secret.properties")
 @ConfigurationProperties(prefix = "secret.client")
+@Getter
+@Setter
 public class ServerSecret {
     private String id;
     private String secret;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
 }
