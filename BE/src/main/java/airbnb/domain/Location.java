d22f318f666cf1e10b6398c19c8fb069aff98f5e
95degree <95degree@naver.com>
2021-05-24 11:11:30 +0900

    feat: ✨위도 경도 계산 메소드 생성(#20)
    어떤 위치의 위도 경도가 주어졌을때 나와 그 위치 사이의 소요시간을 계산

--- BE/src/main/java/airbnb/domain/Location.java
+++ BE/src/main/java/airbnb/domain/Location.java
@@ -2,19 +2,39 @@ package airbnb.domain;
 
 public class Location {
 
-    private int latitude;
-    private int longitude;
+    private double latitude;
+    private double longitude;
+    private static final double STRAIGHT_ANGLE = 180.0;
 
-    public Location(int latitude, int longitude) {
+    public Location(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public int getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
 
-    public int getLongitude() {
+    public double getLongitude() {
         return longitude;
+    }
+
+    public long calculatorTravelTime(Location location) {
+        double theta = this.longitude - location.longitude;
+        double distance = Math.sin(deg2rad(this.latitude)) * Math.sin(deg2rad(location.latitude))
+                + Math.cos(deg2rad(this.latitude)) * Math.cos(deg2rad(location.latitude)) * Math.cos(deg2rad(theta));
+        return Math.round(convertToKilometer(distance));
+    }
+
+    private double deg2rad(double degree) {
+        return (degree * Math.PI / STRAIGHT_ANGLE);
+    }
+
+    private double rad2deg(double radian) {
+        return (radian * STRAIGHT_ANGLE / Math.PI);
+    }
+
+    private double convertToKilometer(double distance) {
+        return (rad2deg(Math.acos(distance)) * 60 * 1.1515 * 1.609344);
     }
 }
