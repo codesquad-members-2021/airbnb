ddcfe864b661c749fe97325e45551de0955a8b79
K <PizzaCola.K@gmail.com>
2021-05-24 11:09:36 +0900

    refactor: LocationDto shows merged address
    - 이제 LocationDto에서 합친 전체 주소를 보여줍니다.
    
    issue: #12

--- BE/src/main/java/codesquad/team17/gnb/place/dto/LocationDto.java
+++ BE/src/main/java/codesquad/team17/gnb/place/dto/LocationDto.java
@@ -20,20 +20,8 @@ public class LocationDto {
         this.longitude = location.getLongitude();
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public String getAddress1() {
-        return address1;
-    }
-
-    public String getAddress2() {
-        return address2;
+    public String getAddress() {
+        return city + " " + district + " " + address1 + " " + address2;
     }
 
     public double getLatitude() {
