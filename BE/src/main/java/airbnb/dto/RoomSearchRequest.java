19d156eb0fdd20a25f5916a03cec7651e0ef8363
95degree <95degree@naver.com>
2021-05-24 11:51:56 +0900

    feat: ✨RoomSearchRequest 생성(#21)
    숙소 검색을 할때 클라이언트가 요청 할 Dto 생성

--- BE/src/main/java/airbnb/dto/RoomSearchRequest.java
+++ BE/src/main/java/airbnb/dto/RoomSearchRequest.java
@@ -1,5 +1,52 @@
 package airbnb.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
 public class RoomSearchRequest {
 
+    @JsonProperty(value = "city")
+    private final Long cityId;
+
+    @JsonProperty(value = "people")
+    private final int maxPersonCount;
+
+    private final int maxPrice;
+    private final int minPrice;
+    private final LocalDateTime checkIn;
+    private final LocalDateTime checkOut;
+
+    public RoomSearchRequest(Long cityId, int maxPersonCount, int maxPrice, int minPrice, LocalDateTime checkIn, LocalDateTime checkOut) {
+        this.cityId = cityId;
+        this.maxPersonCount = maxPersonCount;
+        this.maxPrice = maxPrice;
+        this.minPrice = minPrice;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public int getMaxPersonCount() {
+        return maxPersonCount;
+    }
+
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
 }
