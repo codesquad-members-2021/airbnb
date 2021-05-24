b7520f1c982c903c9e56adec681c0c7589032e32
Hoyoung Son <xzxking17@gmail.com>
2021-05-24 15:11:28 +0900

    add: Main - TopBackground, Header 레이아웃 초기 버전 [#13]

---
+++ FE/src/components/Main/TopBackground/Header.tsx
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import { ResponsiveFluid } from '../../Common/ResponsiveFluid';
+import { FiMenu, FiUser } from 'react-icons/fi';
+import { cssTranslate } from '../../../util/styles/CommonStyledCSS';
+import { ITextTopBackground } from '../../../util/reference';
+
+const Header = ({ headerTexts }: ITextTopBackground) => {
+  const { logoOrBtnCaption: logo, menuItems, authStatus } = headerTexts;
+
+  return (
+    headerTexts && (
+      <HeaderLayout>
+        <LogoBlock>{logo}</LogoBlock>
+        <HeaderMenuList>
+          {menuItems.map((item, i) => (
+            <HeaderMenuItem key={i}>{item.text}</HeaderMenuItem>
+          ))}
+        </HeaderMenuList>
+
+        <AuthButton>
+          <div className="menu">
+            <FiMenu />
+          </div>
+          <div className="user">
+            <FiUser />
+          </div>
+        </AuthButton>
+      </HeaderLayout>
+    )
+  );
+};
+
+export default Header;
+
+// --- Styled Components ---
+const HeaderLayout = styled(ResponsiveFluid)`
+  margin: 0 auto;
+  padding: 24px 0;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoBlock = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.SUBJECT};
+  font-weight: ${({ theme }) => theme.fontWeight.bold2};
+`;
+
+const HeaderMenuList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: ${({ theme }) => theme.fontSize.L};
+`;
+
+const HeaderMenuItem = styled.li`
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray3};
+  }
+  & + & {
+    margin-left: 24px;
+  }
+`;
+
+const AuthButton = styled.button`
+  ${cssTranslate};
+  cursor: pointer;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 8px;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray4};
+  border-radius: 24px;
+  padding: 6px;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+  .menu {
+    font-size: ${({ theme }) => theme.fontSize.XL};
+    padding-left: 6px;
+  }
+  .user {
+    border-radius: 50%;
+    font-size: 16px;
+    background-color: ${({ theme }) => theme.colors.gray3};
+    color: ${({ theme }) => theme.colors.white};
+    padding: 5px;
+  }
+`;
--- FE/src/components/Main/index.tsx
+++ FE/src/components/Main/index.tsx
@@ -1,25 +1,33 @@
+import Background from '../Common/Background';
 import TopBackground from './TopBackground';
 import Nearby from './Nearby';
 import RoomType from './RoomType';
 import ContentInfo from './ContentInfo';
 import Footer from './Footer';
-
-import { Text, TextContentInfo, TextFooter } from '../../util/reference';
-import Background from '../Common/Background';
+import {
+  Text,
+  TextContentInfo,
+  TextFooter,
+  TextTopBackground,
+} from '../../util/reference';
 
 const Main = () => {
   const { nearby, roomType } = Text;
   const { contentInfo } = TextContentInfo;
   const { footerItems } = TextFooter;
+  const { headerTexts, searchBarTexts } = TextTopBackground;
 
   return (
     <>
-      <TopBackground />
+      <TopBackground
+        headerTexts={headerTexts}
+        searchBarTexts={searchBarTexts}
+      />
       <Nearby nearbyItems={nearby} />
       <RoomType roomTypeItems={roomType} />
-      <Background color={'gray6'} >
-        <ContentInfo contentInfoItems={contentInfo}/>
-        <Footer footerItems={footerItems}/>
+      <Background color={'gray6'}>
+        <ContentInfo contentInfoItems={contentInfo} />
+        <Footer footerItems={footerItems} />
       </Background>
     </>
   );
