243392cd3133917d30eaef8cd4bf47b130b2610e
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-24 11:44:27 +0900

    feat: callback 핸들러 메서드 생성 및 코드 획득 확인

--- be/airbnb/src/main/java/com/codesquad/airbnb/web/controller/UsersController.java
+++ be/airbnb/src/main/java/com/codesquad/airbnb/web/controller/UsersController.java
@@ -1,12 +1,15 @@
 package com.codesquad.airbnb.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @RequestMapping("/users")
 @RestController
 public class UsersController {
@@ -14,5 +17,10 @@ public class UsersController {
     @GetMapping("/login")
     public void login(HttpServletResponse response) throws IOException {
         response.sendRedirect("https://github.com/login/oauth/authorize?client_id=Iv1.da513e37628ae941&scope=user");
+    }
+
+    @GetMapping("/callback")
+    public void callback(@RequestParam(value = "code") String code) {
+        log.info("code : {}", code);
     }
 }
