9fa6e98e0e7f1b4c277804f060256596f1245291
zeke <yangsin1031@naver.com>
2021-05-24 22:34:16 +0900

    feat: DateViewController 생성

--- ios/airbnb/airbnb/Controller/DateViewController.swift
+++ ios/airbnb/airbnb/Controller/DateViewController.swift
@@ -6,7 +6,34 @@
 //
 
 import UIKit
+import FSCalendar
 
 class DateViewController: UIViewController {
 
+    @IBOutlet weak var calendarView: FSCalendar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendarView.delegate = self
+        calendarView.dataSource = self
+        setUpCalendarView()
+        navigationItem.title = "숙소 찾기"
+    }
+ 
+    func setUpCalendarView() {
+        calendarView.allowsMultipleSelection = true
+        calendarView.swipeToChooseGesture.isEnabled = true
+        calendarView.scrollDirection = .vertical
+        calendarView.appearance.headerDateFormat = "YYYY년 M월"
+        calendarView.appearance.headerTitleColor = .black
+        calendarView.locale = Locale(identifier: "ko_KR")
+    }
+}
+
+extension DateViewController: FSCalendarDelegate {
+    
+}
+
+extension DateViewController: FSCalendarDataSource {
+    
 }
