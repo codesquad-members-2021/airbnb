303784e66bd7c0730dcf24212c4f38b853020e90
jeonyeonkyu <ehb6915@naver.com>
2021-05-26 01:13:53 +0900

    Feat: 구글맵 API 연동

--- FE/client/src/Components/SearchMap/index.tsx
+++ FE/client/src/Components/SearchMap/index.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { GoogleMap, useJsApiLoader,LoadScript } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, LoadScript } from '@react-google-maps/api';
 import GOOGLE_MAP_API_KEY from '../../utils/googleMapAPIKey';
+
 
 const containerStyle = {
   width: '50vw',
-  height: '90vh'
+  height: '89vh',
 };
 
 const center = {
@@ -14,40 +15,28 @@ const center = {
 };
 
 const SearchMap = () => {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: GOOGLE_MAP_API_KEY
-  })
-  const [map, setMap] = useState(null);
-
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, []);
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, []);
-
   return (
     <SearchMapWrapper>
-     <LoadScript
-      googleMapsApiKey={GOOGLE_MAP_API_KEY}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={13}
+      <LoadScript
+        googleMapsApiKey={GOOGLE_MAP_API_KEY}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={13}
+        >
+          { /* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
     </SearchMapWrapper>
   )
 }
 
 const SearchMapWrapper = styled.div`
+  position: fixed;
+  right:0;
+  top: 6.4rem;
 `;
 
 
