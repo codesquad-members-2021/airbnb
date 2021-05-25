d869983b842c2654e12cd6b00c3b20fad553f51a
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-25 17:56:53 +0900

    chore: 더미데이터 수정

--- be/airbnb/src/main/resources/data.sql
+++ be/airbnb/src/main/resources/data.sql
@@ -11,16 +11,27 @@ VALUES (1, '대한민국', 'NATION', null, 1, 'ChIJzWXFYYuifDUR64Pq5LTtioU', POI
        (10, '연천군', 'COUNTY', 9, 3, 'ChIJiwRU64chfTURJbYgnupP6cM', POINT(37.473553, 127.03067)),
        (11, '미산면', 'TOWNSHIP', 10, 4, 'ChIJ1eYEIfXffDURynFuzSssdWs', POINT(37.473553, 127.03067)),
        (12, '삼화리', 'VILLAGE', 11, 5, 'ChIJD5yTF7_ffDURV9QtTz4n8wI', POINT(37.473553, 127.03067)),
-       (13, '동이리', 'VILLAGE', 11, 5, 'ChIJ01VA53nffDURt9m_xoktSJo', POINT(37.473553, 127.03067))
-;
+       (13, '동이리', 'VILLAGE', 11, 5, 'ChIJ01VA53nffDURt9m_xoktSJo', POINT(37.473553, 127.03067));
 
-insert into room(id, location_id, name, rating, guest_capacity, point, description)
-values (1, 4, '희망이 넘치는 발레노르 움막집', 5, 5, POINT(37.252352, 235.52532), '희망이 넘쳐요'),
-       (2, 4, '분노가 넘치는 블랙게이트 감시초소', 0.5, 3, POINT(31.25144, 935.52532), '오지마세요 제발'),
-       (3, 4, '유혹이 넘치는 바랏두르 제련소', 1.0, 1, POINT(27.252352, 235.52532), '춥진 않아요'),
-       (4, 8, '전망이 멋진 미나스 티리스 펜션', 5, 2, POINT(37.252352, 235.52532), '정말 최고야'),
-       (5, 12, '먼지가 넘치는 모리아 골방', 0.5, 4, POINT(17.252352, 125.52532), '청소가 뭐예요?')
-;
+insert into room(id, location_id, name, rating, guest_capacity, point, description, bathroom_type, bedroom_type,
+                 bed_count, amenity)
+values (1, 4, '희망이 넘치는 발레노르 움막집', 5, 5, POINT(37.252352, 235.52532), '희망이 넘쳐요', 'PRIVATE_BATHROOM', 'ONE_ROOM', 2,
+        '주방, 무선인터넷, 에어컨, 헤어드라이어'),
+       (2, 4, '분노가 넘치는 블랙게이트 감시초소', 0.5, 3, POINT(31.25144, 935.52532), '오지마세요 제발', 'PRIVATE_BATHROOM', 'ONE_ROOM', 2,
+        '주방, 무선인터넷, 에어컨, 헤어드라이어'),
+       (3, 4, '유혹이 넘치는 바랏두르 제련소', 1.0, 1, POINT(27.252352, 235.52532), '춥진 않아요', 'SHARED_BATHROOM', 'BEDROOM', 3,
+        '주방, 무선인터넷, 에어컨, 헤어드라이어'),
+       (4, 12, '전망이 멋진 미나스 티리스 펜션', 5, 2, POINT(37.252352, 235.52532), '정말 최고야', 'PRIVATE_BATHROOM', 'BEDROOM', 4,
+        '주방, 무선인터넷, 에어컨, 헤어드라이어'),
+       (5, 13, '먼지가 넘치는 모리아 골방', 0.5, 4, POINT(17.252352, 125.52532), '청소가 뭐예요?', 'SHARED_BATHROOM', 'BEDROOM', 5,
+        '주방, 무선인터넷, 에어컨, 헤어드라이어');
+
+insert into room_price (room_id, service_fee, accomodation_tax, clean_up_cost, price_per_day)
+values (1, 500, 1000, 2500, 50000),
+       (2, 500, 1000, 2500, 50000),
+       (3, 500, 1000, 2500, 50000),
+       (4, 500, 1000, 2500, 50000),
+       (5, 500, 1000, 2500, 50000);
 
 insert into reservation(id, room_id, guest_id, checkin_date_time, checkout_date_time, adult_count, child_count,
                         infant_count)
