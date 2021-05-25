f2a02f993fa0b124fe06e0b5096690ccf6a2e3d6
eve712 <62237639+eve712@users.noreply.github.com>
2021-05-26 04:09:13 +0900

    feat: 캘린더 구현, 화살표 버튼 클릭에 따라 새로 렌더링

--- FE/airbnb/src/component/searchBar/calendar/Calendar.tsx
+++ FE/airbnb/src/component/searchBar/calendar/Calendar.tsx
@@ -1,102 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import getCalendarData from "component/searchBar/calendar/getCalendarData";
 
 const days: string[] = ["일", "월", "화", "수", "목", "금", "토"];
 
-const getWeekData = (dayCount: number, lastDay: number, firstDayName = 0): [number, string[]] => {
-  const emptyWeek: null[] = new Array(7).fill(null);
-  const week: string[] = emptyWeek.map((_, i) => {
-    if (i < firstDayName || dayCount >= lastDay) return "";
-    dayCount++;
-    return `${dayCount}`;
-  });
-  return [dayCount, week];
-};
+interface Props {
+  year: number;
+  month: number;
+}
 
-const getCalendarData = (year: number, month: number): string[][] => {
-  let weeksData: string[][] = [];
-  const setFirstDate: Date = new Date(year, month - 1, 1);
-  const setLastDate: Date = new Date(year, month, 0);
-  const firstDayName: number = setFirstDate.getDay();
-  const lastDay: number = setLastDate.getDate();
-
-  let dayCount: number = 0;
-
-  for (let i = 0; i < 6; i++) {
-    if (dayCount === 0) {
-      // 첫 주 배열 push
-      const [count, firstWeek] = getWeekData(dayCount, lastDay, firstDayName);
-      dayCount = count;
-      weeksData.push(firstWeek);
-    } else if (dayCount < lastDay) {
-      // 나머지 주 배열 push
-      const [count, restWeek] = getWeekData(dayCount, lastDay);
-      dayCount = count;
-      weeksData.push(restWeek);
-    }
-    if (dayCount >= lastDay) break;
+function Calendar({ year, month }: Props) {
+  if (month < 0) {
+    month += 12;
+    year--;
   }
-  return weeksData;
-};
-
-function Calendar() {
+  if (month > 11) {
+    month -= 12;
+    year++;
+  }
+  const weeksData = getCalendarData(year, month);
   return (
     <CalendarContainer>
-      <CalendarTitle>2021년 5월</CalendarTitle>
+      <CalendarTitle>
+        {year}년 {month + 1}월
+      </CalendarTitle>
       <CalendarTable>
         <thead>
           <tr>
             {days.map((day) => (
-              <th>{day}</th>
+              <th key={day}>{day}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-          </tr>
-          <tr>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-            <td>21</td>
-          </tr>
-          <tr>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-          </tr>
-          <tr>
-            <td>29</td>
-            <td>30</td>
-            <td>31</td>
-            <td>32</td>
-            <td>33</td>
-            <td>34</td>
-            <td>35</td>
-          </tr>
+          {weeksData.map((week) => (
+            <tr>
+              {week.map((day) => (
+                <td>{day}</td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </CalendarTable>
     </CalendarContainer>
@@ -112,7 +55,7 @@ const CalendarContainer = styled.div`
   align-items: center;
 `;
 const CalendarTitle = styled.div`
-  margin-bottom: 24px;
+  margin: 5px 0 24px;
   font-weight: bold;
 `;
 const CalendarTable = styled.table`
--- FE/airbnb/src/component/searchBar/calendar/CalendarModal.tsx
+++ FE/airbnb/src/component/searchBar/calendar/CalendarModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Calendar from "component/searchBar/calendar/Calendar";
 import { ReactComponent as LeftButton } from "assets/left_arrow.svg";
@@ -6,12 +6,39 @@ import { ReactComponent as RightButton } from "assets/right_arrow.svg";
 import { stopPropagation } from "hooks/modalHooks";
 
 function CalendarModal() {
+  // 날짜선택 X → 오늘 날짜 기준으로 달력 렌더
+  const today = new Date();
+  const [year, setYear] = useState<number>(() => today.getFullYear());
+  const [month, setMonth] = useState<number>(() => today.getMonth());
+  // 날짜선택 O → 선택된 체크인 날짜 기준으로 달력 렌더
+
+  const handleClickLeftBtn = (): void => {
+    if (month > 1) setMonth(month - 2);
+    else {
+      setYear(year - 1);
+      setMonth(month + 10);
+    }
+  };
+  const handleClickRightBtn = (): void => {
+    if (month < 10) setMonth(month + 2);
+    else {
+      setYear(year + 1);
+      setMonth(month - 10);
+    }
+  };
+
+  const theNumOfCalendar = new Array(6).fill(null);
   return (
     <Modal onClick={stopPropagation}>
-      <LeftButton className="arrow left" />
-      <Calendar />
-      <Calendar />
-      <RightButton className="arrow right" />
+      <ModalContent>
+        <CalendarWrap>
+          {theNumOfCalendar.map((_, i) => (
+            <Calendar year={year} month={month - 2 + i} />
+          ))}
+        </CalendarWrap>
+      </ModalContent>
+      <LeftButton className="arrow left" onClick={handleClickLeftBtn} />
+      <RightButton className="arrow right" onClick={handleClickRightBtn} />
     </Modal>
   );
 }
@@ -24,12 +51,9 @@ const Modal = styled.div`
   left: 0;
   box-sizing: border-box;
   width: 916px;
-  padding: 65px 88px;
+  padding: 60px 80px;
   background-color: #fff;
   border-radius: ${({ theme }) => theme.borderRadius.calendar};
-
-  display: flex;
-  justify-content: space-between;
 
   .arrow {
     position: absolute;
@@ -50,4 +74,16 @@ const Modal = styled.div`
   .right {
     right: 90px;
   }
+`;
+
+const ModalContent = styled.div`
+  width: 100%;
+  /* overflow: hidden; */
+`;
+
+const CalendarWrap = styled.div`
+  width: 300%;
+  transform: translateX(calc(-100% / 3));
+  display: flex;
+  justify-content: space-between;
 `;
