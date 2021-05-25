08d29c82ee71b4262eb6106671c90ccfd4f2474c
Sally Oh <min27604@hotmail.com>
2021-05-25 12:31:35 +0900

    test: 평균 가격을 구하는 averagePriceTest 코드 구현

---
+++ BE/src/test/java/com/codesquad/airbnb/service/PropertyServiceTest.java
@@ -0,0 +1,31 @@
+package com.codesquad.airbnb.service;
+
+import com.codesquad.airbnb.dto.PriceSearchDTO;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class PropertyServiceTest {
+
+    private Logger logger = LoggerFactory.getLogger(PropertyServiceTest.class);
+
+    private PropertyService propertyService;
+
+    @Autowired
+    public PropertyServiceTest(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    @Test
+    void propertyService_averagePriceTest() {
+        int averagePrice = propertyService.getAveragePrice(1L);
+
+        assertThat(averagePrice).isGreaterThan(0);
+        logger.info("Average Price of location 1L is: " + averagePrice);
+    }
+}
