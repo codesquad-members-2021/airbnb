bfebe331b0e3787912b8a8905d6efa669988e30f
bong6981 <kbk5190@naver.com>
2021-05-25 21:20:46 +0900

    feat: booking 목록 가져오는 기능 구현(#15)

--- be/airbnb/src/main/java/com/team19/airbnb/controller/BookingController.java
+++ be/airbnb/src/main/java/com/team19/airbnb/controller/BookingController.java
@@ -1,17 +1,14 @@
 package com.team19.airbnb.controller;
 
 import com.team19.airbnb.ResponseBody;
-import com.team19.airbnb.domain.room.Host;
 import com.team19.airbnb.dto.BookingRequestDTO;
 import com.team19.airbnb.dto.BookingResponseDTO;
 import com.team19.airbnb.service.BookingService;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
+@RequestMapping("/bookings")
 @RestController
 public class BookingController {
 
@@ -21,22 +18,22 @@ public class BookingController {
         this.bookingService = bookingService;
     }
 
-    @PostMapping("/bookings/{userId}")
-    public void bookRoom(@RequestBody BookingRequestDTO bookingRequestDTO) {
+    @PostMapping("/{userId}")
+    public void book(@RequestBody BookingRequestDTO bookingRequestDTO) {
         System.out.println(bookingRequestDTO.toString());
     }
 
-    @GetMapping("/bookings/{userId}")
-    public List<BookingResponseDTO> getBookings() {
-        return null;
+    @GetMapping("/{userId}")
+    public ResponseBody<List<BookingResponseDTO>> showBookings(@PathVariable Long userId) {
+        return ResponseBody.ok(bookingService.showBookings(userId));
     }
 
-    @GetMapping("/bookings/{bookingId}/{userId}")
-    public ResponseBody<BookingResponseDTO> getBooking(@PathVariable Long bookingId, @PathVariable Long userId) {
-        return ResponseBody.ok(bookingService.findBooking(userId, bookingId));
+    @GetMapping("/{bookingId}/{userId}")
+    public ResponseBody<BookingResponseDTO> showBooking(@PathVariable Long bookingId, @PathVariable Long userId) {
+        return ResponseBody.ok(bookingService.showBooking(userId, bookingId));
     }
 
-    @DeleteMapping("/bookings/{bookingId}/{userId}")
+    @DeleteMapping("/{bookingId}/{userId}")
     public void deleteWishList(@PathVariable Long bookingId, @PathVariable Long userId) {
     }
 }
--- be/airbnb/src/main/java/com/team19/airbnb/service/BookingService.java
+++ be/airbnb/src/main/java/com/team19/airbnb/service/BookingService.java
@@ -2,27 +2,52 @@ package com.team19.airbnb.service;
 
 import com.team19.airbnb.domain.Booking;
 import com.team19.airbnb.domain.User;
+import com.team19.airbnb.domain.room.Room;
+import com.team19.airbnb.dto.BookingResponseDTO;
 import com.team19.airbnb.exception.BookingNotFoundException;
+import com.team19.airbnb.exception.RoomNotFoundException;
 import com.team19.airbnb.exception.UserNotFoundException;
 import com.team19.airbnb.repository.BookingDAO;
+import com.team19.airbnb.repository.RoomRepository;
 import com.team19.airbnb.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookingService {
 
     private final BookingDAO bookingDAO;
     private final UserRepository userRepository;
+    private final RoomRepository roomRepository;
 
-    public BookingService(BookingDAO bookingDAO, UserRepository userRepository) {
+    public BookingService(BookingDAO bookingDAO, UserRepository userRepository, RoomRepository roomRepository) {
         this.bookingDAO = bookingDAO;
         this.userRepository = userRepository;
+        this.roomRepository = roomRepository;
     }
 
-    public void findBooking(Long userId, Long bookingId) {
+    public BookingResponseDTO showBooking(Long userId, Long bookingId) {
+        //userService를 참조할지 userRepository를 참조할지 고민 (삭제할 부분이라 repository 참조)
+        //OAuth 구현식 삭제될 user 체크
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
         Booking booking = bookingDAO.findById(bookingId).orElseThrow(BookingNotFoundException::new);
+        return toResponseDTO(booking);
+    }
+
+    public List<BookingResponseDTO> showBookings(Long userId) {
+        //OAuth 구현식 삭제될 user 체크
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        return bookingDAO.findAllByUser(userId).stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    private BookingResponseDTO toResponseDTO(Booking booking) {
+        Room room = roomRepository.findById(booking.getRoom()).orElseThrow(RoomNotFoundException::new);
+        return new BookingResponseDTO.Builder(booking, room).build();
     }
 }
