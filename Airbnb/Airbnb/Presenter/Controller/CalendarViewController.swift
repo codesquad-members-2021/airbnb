df6ecbadbfffb65a4e8eb942d062ff370cbad9ac
Ray suh <readingod@naver.com>
2021-05-25 17:12:07 +0900

    feat: Add BackVC Button (#14)

--- Airbnb/Airbnb/Presenter/Controller/CalendarViewController.swift
+++ Airbnb/Airbnb/Presenter/Controller/CalendarViewController.swift
@@ -3,12 +3,13 @@ import FSCalendar
 
 class CalendarViewController: UIViewController {
     
-    private var locationInfo:String?
+    @IBOutlet var backButton: UIButton!
     @IBOutlet var calendarView: FSCalendar!
     
+    private var locationInfo:String?
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCalendarView()
+        setupMainView()
     }
     
     func setupLocation(_ info:String) {
@@ -18,10 +19,22 @@ class CalendarViewController: UIViewController {
 
 private extension CalendarViewController {
     
+    private func setupMainView() {
+        setupCalendarView()
+        setupButton()
+    }
+    
     private func setupCalendarView() {
         calendarView.allowsMultipleSelection = true
         calendarView.swipeToChooseGesture.isEnabled = true
         calendarView.appearance.selectionColor = UIColor.systemGray3
         calendarView.appearance.todayColor = UIColor.black
+    }
+    
+    private func setupButton() {
+        backButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.dismiss(animated: true, completion: nil)
+            }).disposed(by: rx.disposeBag)
     }
 }
