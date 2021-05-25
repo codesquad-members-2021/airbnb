f9ca665e9193741cbd17c23eaddb79ecf2151d8a
95degree <95dgree@naver.com>
2021-05-25 15:27:42 +0900

    feat : ✨RoomMapper 생성(#21)

---
+++ BE/src/main/java/airbnb/mapper/RoomMapper.java
@@ -0,0 +1,26 @@
+package airbnb.mapper;
+
+import airbnb.domain.*;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RoomMapper implements RowMapper<Room> {
+    @Override
+    public Room mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Room.Builder(rs.getLong("id")).price(rs.getInt("price"))
+                .title(rs.getString("title")).description(rs.getString("description"))
+                .roomDetail(createRoomDetail(rs)).tax(createTax(rs)).build();
+    }
+
+    private RoomDetail createRoomDetail(ResultSet rs) throws SQLException {
+        return new RoomDetail.Builder().oneRoom(rs.getBoolean("oneRoom")).bedCount(rs.getInt("bed")).bathCount(rs.getInt("bath"))
+                .hairDryer(rs.getBoolean("hair_dryer")).airConditioner(rs.getBoolean("air_conditioner"))
+                .wiFi(rs.getBoolean("wiFi")).build();
+    }
+
+    private Tax createTax(ResultSet rs) throws SQLException {
+        return new Tax(rs.getInt("clean_tax"), rs.getInt("service_tax"), rs.getInt("accommodation_tax"));
+    }
+}
