11f0267dfe3595512ebf82f1fc8ed9dbeea2be3b
jeonyeonkyu <ehb6915@naver.com>
2021-05-26 01:10:45 +0900

    Fix: 헤더의 포지션을 fixed로 변경함에 따라 top 위치 조정

--- FE/client/src/Components/HotelList/index.tsx
+++ FE/client/src/Components/HotelList/index.tsx
@@ -15,10 +15,12 @@ const HotelList = () => {
 }
 
 const HotelListWrapper = styled.div`
+  position:relative;
   width: 50%;
   min-width: 30rem;
   padding: 2rem 1.5rem 0;
   height: 100%;
+  top:6.4rem;
 `;
 
 export default HotelList;
