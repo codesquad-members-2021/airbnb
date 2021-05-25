32baea8892479ae5cf1e54c11027993725951fe9
dong <ultraraptor@naver.com>
2021-05-25 23:20:13 +0900

    사용가능한 숙소 리스트에서 검색조건에 맞는 숙소를 반영할수 있는 코드 추가

--- BE/airbnb/src/main/java/com/example/airbnb/service/AccommodationService.java
+++ BE/airbnb/src/main/java/com/example/airbnb/service/AccommodationService.java
@@ -1,17 +1,15 @@
 package com.example.airbnb.service;
 
 import com.example.airbnb.domain.Accommodation;
-import com.example.airbnb.utils.SearchConditions;
 import com.example.airbnb.dto.AccommodationDTO;
 import com.example.airbnb.dto.AccommodationListDTO;
 import com.example.airbnb.repository.AccommodationDAO;
+import com.example.airbnb.utils.SearchConditions;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.example.airbnb.utils.StringToLocalDate.StringToLocalDate;
 
 @Service
 public class AccommodationService {
@@ -22,50 +20,54 @@ public class AccommodationService {
         this.accommodationDAO = accommodationDAO;
     }
 
-
     public AccommodationListDTO availableAccommodationsList(SearchConditions conditions) {
-        SearchConditions sanitizedConditions = conditionVaildation(conditions);
         List<Accommodation> accommodationList = accommodationDAO.findAll();
-        return domainListToDTOList(accommodationList);
-
+        return domainListToDTOList(conditionValidate(accommodationList, conditions));
     }
 
-    private SearchConditions conditionVaildation(SearchConditions conditions) {
-        if((conditions.getCheckout() != null ) && (conditions.getCheckin()!=null) ){
-            LocalDate checkinDate = StringToLocalDate(conditions.getCheckin());
-            LocalDate checkoutDate = StringToLocalDate(conditions.getCheckout());
-
+    private List<Accommodation> conditionValidate(List<Accommodation> accommodationList, SearchConditions conditions) {
+        if (isDateValidated(conditions)) {
+            accommodationList = filterdDate(accommodationList);
         }
-        else {
-
+        if (isPeopleCountValidated(conditions)) {
+            accommodationList = filterdPeopleCount(accommodationList);
         }
+        return accommodationList;
+    }
 
+    private boolean isPeopleCountValidated(SearchConditions conditions) {
+        if (0 >= conditions.getAdults()) {
+            return false;
+        }
+        if (0 >= conditions.getChildren()) {
+            return false;
+        }
+        if (0 >= conditions.getInfants()) {
+            return false;
+        }
+        return true;
+    }
 
+    private List<Accommodation> filterdPeopleCount(List<Accommodation> accommodationList) {
+        return accommodationList;
+    }
 
+    private boolean isDateValidated(SearchConditions conditions) {
+        LocalDate checkin = LocalDate.parse(conditions.getCheckin());
+        LocalDate checkout = LocalDate.parse(conditions.getCheckin());
+        return (checkin.isBefore(checkout) || checkin.isEqual(checkout));
+    }
+
+    private List<Accommodation> filterdDate(List<Accommodation> accommodationList) {
+        return accommodationList;
     }
 
     private AccommodationListDTO domainListToDTOList(List<Accommodation> accommodationList) {
         List<AccommodationDTO> accommodationDTOList = new ArrayList<>();
-        for(Accommodation accommodation : accommodationList) {
+        for (Accommodation accommodation : accommodationList) {
             accommodationDTOList.add(new AccommodationDTO(accommodation));
         }
         return new AccommodationListDTO(accommodationDTOList);
-/*
-    public AccommodationListDTO availableAccommodationsList(SearchConditions conditions) {
-
-        List<Accommodation> accommodationList = accommodationDAO.findAll();
-        List<AccommodationDTO> accommodationDTOList = new ArrayList<>();
-
-        for(Accommodation accommodation : accommodationList) {
-            accommodationDTOList.add(new AccommodationDTO(accommodation));
-        }
-
-        // @todo : DB가 준비되면 SearchConditions 을 통해 전달된 데이터를 통해서 조건에 맞는 숙소만 전달해준다
-        
-        return (new AccommodationListDTO(accommodationDTOList, conditions));
-// w >>>>>>> dev-BE
-
- */
     }
 
     public AccommodationListDTO likeMarkredList() {
@@ -73,22 +75,18 @@ public class AccommodationService {
         return domainListToDTOList(markredList);
     }
 
-    public Integer countAccommodation(){
-        return accommodationDAO.countAllAccommodation();
-    }
-
-    public Accommodation findById(Long id){
+    public Accommodation findById(Long id) {
         return accommodationDAO.findById(id);
     }
 
-    public String insert(String title){
+    public String insert(String title) {
         Accommodation accommodation = new Accommodation();
         accommodation.setTitle(title);
         accommodationDAO.insert(accommodation);
         return "OK";
     }
 
-    public String delete(String title){
+    public String delete(String title) {
         Accommodation accommodation = new Accommodation();
         accommodation.setTitle(title);
         accommodationDAO.delete(accommodation);
