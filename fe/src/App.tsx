e9caec8a7bcd82ceb855ee635ea5ff3f2e6338f2
adelakim <diddnjs02@likelion.org>
2021-05-25 13:18:23 +0900

    modify: ReservationState 위치 이동
    - App -> Hero로 옮김
    - 불필요한 리렌더가 많은 것 같고, 여기에 있어도 괜찮을 것 같아서 수정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -7,39 +7,39 @@ import { ReservationStateContext, ReservationDispatchContext } from './Contexts'
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 
-const initialState = {
-    location: {
-        id: 0,
-        city: '',
-    },
-    checkIn: {
-        year: 0,
-        month: 0,
-        day: 0,
-    },
-    checkOut: {
-        year: 0,
-        month: 0,
-        day: 0,
-    },
-    fee: 0,
-    people: {
-        guest: 0,
-        kids: 0,
-    },
-} as ReservationContext;
+// const initialState = {
+//     location: {
+//         id: 0,
+//         city: '',
+//     },
+//     checkIn: {
+//         year: 0,
+//         month: 0,
+//         day: 0,
+//     },
+//     checkOut: {
+//         year: 0,
+//         month: 0,
+//         day: 0,
+//     },
+//     fee: 0,
+//     people: {
+//         guest: 0,
+//         kids: 0,
+//     },
+// } as ReservationContext;
 
 function App(): React.ReactElement {
-    const [reservationState, reservationDispatch] = useReducer(reservationReducer, initialState);
+    // const [reservationState, reservationDispatch] = useReducer(reservationReducer, initialState);
 
     return (
-        <ReservationDispatchContext.Provider value={reservationDispatch}>
-            <ReservationStateContext.Provider value={reservationState}>
-                <ThemeProvider theme={theme}>
-                    <Router />
-                </ThemeProvider>
-            </ReservationStateContext.Provider>
-        </ReservationDispatchContext.Provider>
+        // <ReservationDispatchContext.Provider value={reservationDispatch}>
+        //     <ReservationStateContext.Provider value={reservationState}>
+        <ThemeProvider theme={theme}>
+            <Router />
+        </ThemeProvider>
+        //     </ReservationStateContext.Provider>
+        // </ReservationDispatchContext.Provider>
     );
 }
 
--- fe/src/components/home/mainComponents/Hero.tsx
+++ fe/src/components/home/mainComponents/Hero.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
+import { ReservationDispatchContext, ReservationStateContext } from '../../../Contexts';
+import { ReservationContext } from '../../../shared/interface';
+import reservationReducer from '../../../shared/reservationReducer';
 import Header from './heroComponents/Header';
 import Searcher from './heroComponents/Searcher';
 
+const initialState = {
+    location: {
+        id: 0,
+        city: '',
+    },
+    checkIn: {
+        year: 0,
+        month: 0,
+        day: 0,
+    },
+    checkOut: {
+        year: 0,
+        month: 0,
+        day: 0,
+    },
+    fee: 0,
+    people: {
+        guest: 0,
+        kids: 0,
+    },
+} as ReservationContext;
+
 const Hero = (): React.ReactElement => {
+    const [reservationState, reservationDispatch] = useReducer(reservationReducer, initialState);
+
     return (
-        <HeroSection>
-            <Header />
-            <Searcher />
-        </HeroSection>
+        <ReservationDispatchContext.Provider value={reservationDispatch}>
+            <ReservationStateContext.Provider value={reservationState}>
+                <HeroSection>
+                    <Header />
+                    <Searcher />
+                </HeroSection>
+            </ReservationStateContext.Provider>
+        </ReservationDispatchContext.Provider>
     );
 };
 
