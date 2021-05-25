57ad91a193e6e196b105fe423f1c7b54a014c104
zeke <yangsin1031@naver.com>
2021-05-25 17:45:08 +0900

    feat: EndPoint 수정

--- ios/airbnb/airbnb/Model/EndPoint.swift
+++ ios/airbnb/airbnb/Model/EndPoint.swift
@@ -14,16 +14,45 @@ enum EndPoint {
     
     enum Path: String {
         case cities = "/cities"
+        case rooms, reservation
+        case price = "/rooms/prices"
+        
+        func assignRoom(id: Int) -> String {
+            switch self {
+            case .rooms:
+                return "/rooms/\(id)"
+            default:
+                break
+            }
+            return ""
+        }
+        
+        func assignReservation(id: Int) -> String {
+            switch self {
+            case .reservation:
+                return "/reservations/\(id)"
+            default:
+                break
+            }
+            return ""
+        }
     }
     
-    static func url(path: Path, queryItems: [URLQueryItem]? = nil) -> String {
+    static func url(path: Path, queryItems: [URLQueryItem]? = nil, reservationRoomId: Int? = nil, roomId: Int? = nil) -> String {
         var component = URLComponents()
         component.scheme = scheme
         component.host = host
         component.port = port
         component.path = {
             switch path {
-            case .cities : return path.rawValue
+            case .cities:
+                return path.rawValue
+            case .rooms:
+                return path.assignRoom(id: roomId ?? 0)
+            case .reservation:
+                return path.assignReservation(id: reservationRoomId ?? 0)
+            case .price:
+                return path.rawValue
             }
         }()
         component.queryItems = queryItems
@@ -36,8 +65,11 @@ enum QueryItems {
     case checkIn
     case checkOut
     case cityName
+    case maxPrice
+    case minPrice
+    case numOfPeople
     
-    func assign(value: String) -> URLQueryItem {
+    private func assign(value: String) -> URLQueryItem {
         switch self {
         case .checkIn:
             return URLQueryItem(name: "checkIn", value: value)
@@ -45,6 +77,12 @@ enum QueryItems {
             return URLQueryItem(name: "checkOut", value: value)
         case .cityName:
             return URLQueryItem(name: "cityName", value: value)
+        case .maxPrice:
+            return URLQueryItem(name: "maxPrice", value: value)
+        case .minPrice:
+            return URLQueryItem(name: "minPrice", value: value)
+        case .numOfPeople:
+            return URLQueryItem(name: "numOfPeople", value: value)
         }
     }
 }
