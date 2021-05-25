bbbd296a985de00082280684ea0092a4939a9187
Ray suh <readingod@naver.com>
2021-05-25 17:05:32 +0900

    feat: Setup CalendarView (#14)

--- Airbnb/Airbnb/Presenter/Controller/CalendarViewController.swift
+++ Airbnb/Airbnb/Presenter/Controller/CalendarViewController.swift
@@ -4,13 +4,24 @@ import FSCalendar
 class CalendarViewController: UIViewController {
     
     private var locationInfo:String?
+    @IBOutlet var calendarView: FSCalendar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(locationInfo)
+        setupCalendarView()
     }
     
     func setupLocation(_ info:String) {
         locationInfo = info
+    }
+}
+
+private extension CalendarViewController {
+    
+    private func setupCalendarView() {
+        calendarView.allowsMultipleSelection = true
+        calendarView.swipeToChooseGesture.isEnabled = true
+        calendarView.appearance.selectionColor = UIColor.systemGray3
+        calendarView.appearance.todayColor = UIColor.black
     }
 }
