81924e6a65f7f63050877ffbb2cc91fb419580e7
Tree <gmldbs1109@naver.com>
2021-05-25 16:42:25 +0900

    chore : 16브랜치에서 pull한 코드 커밋

--- be/airbnb/src/test/java/com/team19/airbnb/repository/BookingRepositoryTest.java
+++ be/airbnb/src/test/java/com/team19/airbnb/repository/BookingRepositoryTest.java
@@ -22,5 +22,38 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class BookingRepositoryTest {
-  
+    private static final Logger logger = LoggerFactory.getLogger(BookingRepositoryTest.class);
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BookingDAO bookingDAO;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Test
+    @DisplayName("booking을 저장한다")
+    void saveBooking() {
+        //Given
+        User user = User.create("github");
+        userRepository.save(user);
+        Room room = Room.create("room", new ArrayList<>(),
+                1.0, 1,
+                Location.create("address", 1.0, 1.0),
+                new BigDecimal(1000),
+                "roomType", "roomConfiguration", "description",
+                Host.create("tree", "treeImage"));
+        roomRepository.save(room);
+
+        //When
+       Booking savedBooking = bookingDAO.save(Booking.create(LocalDate.now(), LocalDate.now(),
+                50, BigDecimal.valueOf(1000.0),
+                1L, 1L));
+
+        //Then
+        logger.debug("SavedBooking : {}", savedBooking);
+        assertThat(bookingDAO.findById(1L).get().getGuest()).isEqualTo(50);
+    }
 }
