a75fd29f260413a0dbf48e652195b3fadcdb22f7
95degree <95dgree@naver.com>
2021-05-25 15:28:05 +0900

    feat : ✨Tax 생성(#21)

--- BE/src/main/java/airbnb/domain/Tax.java
+++ BE/src/main/java/airbnb/domain/Tax.java
@@ -1,8 +1,25 @@
 package airbnb.domain;
 
 public class Tax {
-
     private int cleanTax;
     private int serviceTax;
     private int accommodationTax;
+
+    public Tax(int cleanTax, int serviceTax, int accommodationTax) {
+        this.cleanTax = cleanTax;
+        this.serviceTax = serviceTax;
+        this.accommodationTax = accommodationTax;
+    }
+
+    public int getCleanTax() {
+        return cleanTax;
+    }
+
+    public int getServiceTax() {
+        return serviceTax;
+    }
+
+    public int getAccommodationTax() {
+        return accommodationTax;
+    }
 }
