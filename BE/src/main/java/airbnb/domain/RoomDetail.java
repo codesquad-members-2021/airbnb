6f7bc859f40d19dd33271ce5c742435c3f2970a2
95degree <95dgree@naver.com>
2021-05-25 15:27:55 +0900

    feat : ✨RoomDetail 생성(#21)

--- BE/src/main/java/airbnb/domain/RoomDetail.java
+++ BE/src/main/java/airbnb/domain/RoomDetail.java
@@ -7,4 +7,59 @@ public class RoomDetail {
     private boolean hairDryer;
     private boolean airConditioner;
     private boolean wiFi;
+
+
+    public RoomDetail(Builder builder) {
+        this.oneRoom = builder.oneRoom;
+        this.bedCount = builder.bedCount;
+        this.bathCount = builder.bathCount;
+        this.hairDryer = builder.hairDryer;
+        this.airConditioner = builder.airConditioner;
+        this.wiFi = builder.wiFi;
+    }
+
+    public static class Builder {
+        private boolean oneRoom;
+        private int bedCount;
+        private int bathCount;
+        private boolean hairDryer;
+        private boolean airConditioner;
+        private boolean wiFi;
+
+
+        public Builder oneRoom(boolean oneRoom) {
+            this.oneRoom = oneRoom;
+            return this;
+        }
+
+        public Builder bedCount(int bedCount) {
+            this.bedCount = bedCount;
+            return this;
+        }
+
+        public Builder bathCount(int bathCount) {
+            this.bathCount = bathCount;
+            return this;
+        }
+
+        public Builder hairDryer(boolean hairDryer) {
+            this.hairDryer = hairDryer;
+            return this;
+        }
+
+        public Builder airConditioner(boolean airConditioner) {
+            this.airConditioner = airConditioner;
+            return this;
+        }
+
+        public Builder wiFi(boolean wiFi) {
+            this.wiFi = wiFi;
+            return this;
+        }
+
+        public RoomDetail build() {
+            return new RoomDetail(this);
+        }
+
+    }
 }
