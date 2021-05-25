2e2ffc133424e4676e57745cf96ce871bf5e222e
jeonyeonkyu <ehb6915@naver.com>
2021-05-26 01:13:35 +0900

    Feat: x버튼 클릭시 요금모달 범위 초기화 추가

--- FE/client/src/Components/SearchBar/Charge.tsx
+++ FE/client/src/Components/SearchBar/Charge.tsx
@@ -3,19 +3,27 @@ import { DivisionContent, DivisionTitle } from './../commons/base';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { IconButton } from "@material-ui/core";
 import { ChargeType } from '../commons/searchBarType';
+import { useRecoilState } from 'recoil';
+import { RangeAtom } from '../../recoil/atoms';
 
 type ChargeModalType = ChargeType & {
   handleClickShowModal: (clickTarget: string) => () => void;
 }
 
 const Charge = ({ handleClickShowModal, charge }: ChargeModalType) => {
+  const [, setRangeState] = useRecoilState(RangeAtom);
+
+  const handleClickResetCharge = () => {
+    setRangeState({left: 0, right:100});
+  }
+
   return (
     <ChargeWrapper onClick={handleClickShowModal('charge')}>
       <span>
         <DivisionTitle>요금</DivisionTitle>
         <DivisionContent>금액대 설정</DivisionContent>
       </span>
-      <IconButton style={{ visibility: charge ? 'visible' : 'hidden' }}>
+      <IconButton style={{ visibility: charge ? 'visible' : 'hidden' }} onClick={handleClickResetCharge}>
         <HighlightOffIcon />
       </IconButton>
     </ChargeWrapper>
