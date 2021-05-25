6ffe52cb56334693635a2d1116aa832835bc0493
DHKim <ultraraptor@naver.com>
2021-05-25 16:01:42 +0900

    문자열에서 날짜로 변경할때 에러가 있는경우 런타임 익셉션을 발동시켜주는 클래스

---
+++ BE/airbnb/src/main/java/com/example/airbnb/exception/UnExpectedTimeFormat.java
@@ -0,0 +1,9 @@
+package com.example.airbnb.exception;
+
+public class UnExpectedTimeFormat extends RuntimeException{
+    private static final String UNEXPECTEDTIMEFORMAT = "this Time-string-Format does not support this applications";
+
+    public UnExpectedTimeFormat() {
+        super(UNEXPECTEDTIMEFORMAT);
+    }
+}
