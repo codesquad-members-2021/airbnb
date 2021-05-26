6bb7669ce24e9b51390735781d0ca3dda0137e98
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 00:48:09 +0900

    refactor : fix sql statement and method according to changed properties

--- src/main/java/com/codesquad/airbnb/repository/BookingRepository.java
+++ src/main/java/com/codesquad/airbnb/repository/BookingRepository.java
@@ -19,38 +19,28 @@ public class BookingRepository implements JdbcRepository<Booking> {
 
     @Override
     public Optional<Booking> findById(Long bookingId) {
-        String sql = "select `id`, `room_id`,`user_id`, `check_in`, `check_out`, `adult`, `child`, `baby` from booking where id = ?";
+        String sql = "select `id`, `room_id`,`user_id`, `check_in`, `check_out`, `number_of_people`, `total_price` from booking where id = ?";
         List<Booking> result = jdbcTemplate.query(sql, bookingRowMapper(), bookingId);
         return result.stream().findAny();
     }
 
     @Override
     public List<Booking> findAll() {
-        String sql = "select `id`, `room_id`, `user_id`, `check_in`, `check_out`, `adult`, `child`, `baby` from booking";
+        String sql = "select `id`, `room_id`, `user_id`, `check_in`, `check_out`, `number_of_people`, `total_price` from booking";
         return jdbcTemplate.query(sql, bookingRowMapper());
     }
 
-    @Override
     public void insert(Booking booking) {
-        String sql = "insert into `booking` (`room_id`,`user_id`, `check_in`, `check_out`, `adult`, `child`, `baby`) values (?, ?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, booking.getRoomId(), booking.getUserId(), booking.getCheckIn(), booking.getCheckOut(), booking.getAdult(), booking.getChild(), booking.getBaby());
-    }
-
-    @Override
-    public void update(Booking booking, Long id) {
-
-    }
-
-    @Override
-    public void delete(Long id) {
-
+        String sql = "insert into `booking` (`room_id`,`user_id`, `check_in`, `check_out`, `number_of_people`, `total_price`) values (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, booking.getRoomId(), booking.getUserId(), booking.getCheckIn(),
+                booking.getCheckOut(), booking.getNumberOfPeople(), booking.getTotalPrice());
     }
 
     private RowMapper<Booking> bookingRowMapper() {
         return (resultSet, rowNum) -> {
             Booking booking = new Booking(resultSet.getLong("id"), resultSet.getLong("room_id"), resultSet.getLong("user_id"),
-                    resultSet.getString("check_in"), resultSet.getString("check_out"), resultSet.getInt("adult"),
-                    resultSet.getInt("child"), resultSet.getInt("baby"));
+                    resultSet.getString("check_in"), resultSet.getString("check_out"), resultSet.getInt("number_of_people"),
+                    resultSet.getInt("total_price"));
             return booking;
         };
     }
