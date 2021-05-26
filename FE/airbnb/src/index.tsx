9235789408c24bfb8059c77b0e285ad283828411
eve712 <62237639+eve712@users.noreply.github.com>
2021-05-27 01:27:44 +0900

    chore: Root 삭제하고 App으로 변경

--- FE/airbnb/src/index.tsx
+++ FE/airbnb/src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "styles/reset.css";
-import Root from "client/Root";
+import App from "App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
