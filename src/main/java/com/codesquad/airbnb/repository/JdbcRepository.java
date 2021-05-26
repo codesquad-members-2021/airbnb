b9dec3af00abe4f36d74728441bb7805d6f7c408
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 00:33:50 +0900

    remove : remove useless abstract method

--- src/main/java/com/codesquad/airbnb/repository/JdbcRepository.java
+++ src/main/java/com/codesquad/airbnb/repository/JdbcRepository.java
@@ -8,10 +8,4 @@ public interface JdbcRepository<T> {
     Optional<T> findById(Long id);
 
     List<T> findAll();
-
-    void insert(T t);
-
-    void update(T t, Long id);
-
-    void delete(Long id);
 }
