df033a09dc4c7a677a67b8164bebfbe51012d5e4
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 00:41:58 +0900

    add : add showing filtered rooms by searching conditions

--- src/main/java/com/codesquad/airbnb/controller/RoomController.java
+++ src/main/java/com/codesquad/airbnb/controller/RoomController.java
@@ -1,16 +1,18 @@
 package com.codesquad.airbnb.controller;
 
-import com.codesquad.airbnb.domain.Room;
+import com.codesquad.airbnb.dto.RoomDTO;
 import com.codesquad.airbnb.dto.Rooms;
 import com.codesquad.airbnb.repository.RoomRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/rooms")
+@RequestMapping("/api")
 public class RoomController {
 
     private RoomRepository roomRepository;
@@ -19,16 +21,22 @@ public class RoomController {
         this.roomRepository = roomRepository;
     }
 
-    @GetMapping
+    @GetMapping("/rooms")
     public Rooms showRooms() {
-        List<Room> rooms = roomRepository.findAll();
-        return new Rooms(rooms);
+        List<RoomDTO> roomDTOS = roomRepository.findAll().stream()
+                .map(RoomDTO::toRoomDTO)
+                .collect(Collectors.toList());
+        return new Rooms(roomDTOS);
     }
 
-//    @GetMapping
-//    public void searchRooms(@RequestParam("checkIn") Date checkIn, @RequestParam("checkOut") Date checkOut,
-//                             @RequestParam("minPrice") int minPrice, @RequestParam("maxPrice") int maxPrice,
-//                             @RequestParam("numberOfPeople") int numberOfPeople) {
-////        return new Rooms(rooms);
-//    }
+    //Todo: 어노테이션 찾아보기
+    @GetMapping("/filteredRooms")
+    public Rooms searchRooms(@RequestParam("checkIn") String checkIn, @RequestParam("checkOut") String checkOut,
+                             @RequestParam("minPrice") int minPrice, @RequestParam("maxPrice") int maxPrice,
+                             @RequestParam("numberOfPeople") int numberOfPeople) {
+        List<RoomDTO> roomDTOS = roomRepository.getFilteredRooms(checkIn, checkOut, minPrice, maxPrice, numberOfPeople).stream()
+                .map(RoomDTO::toRoomDTO)
+                .collect(Collectors.toList());
+        return new Rooms(roomDTOS);
+    }
 }
