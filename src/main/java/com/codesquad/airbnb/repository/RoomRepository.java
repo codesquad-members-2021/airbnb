087ec8e4811794642ba538832281437ca7e236f5
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 00:40:10 +0900

    add : add finding objects of other tables and RowMapper

--- src/main/java/com/codesquad/airbnb/repository/RoomRepository.java
+++ src/main/java/com/codesquad/airbnb/repository/RoomRepository.java
@@ -1,13 +1,14 @@
 package com.codesquad.airbnb.repository;
 
-import com.codesquad.airbnb.domain.Room;
-import com.codesquad.airbnb.domain.RoomBuilder;
+import com.codesquad.airbnb.domain.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class RoomRepository implements JdbcRepository<Room> {
@@ -18,8 +19,7 @@ public class RoomRepository implements JdbcRepository<Room> {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Room> getRoomsByDateAndPriceAndNumberOfPeople(String checkIn, String checkOut, int minPrice, int maxPrice, int numberOfPeople) {
-        // 조인한 BookingRoom 에서 booking의 checkIn, checkOut이 매개변수로 받은 체크인 체크아웃 범위에 들어가있지 않은 room들만 반환
+    public List<Room> getFilteredRooms(String checkIn, String checkOut, int minPrice, int maxPrice, int numberOfPeople) {
         String sql = "select room.id, `max`, `name`, `rating`, `latitude`, `longitude`, `bedroom_count`, `bed_count`, " +
                 "`bathroom_count`, `address`, `detail_address`, `comment_count`, `original_price`, `sale_price`, " +
                 "`flexible_refund`, `immediate_booking` from room " +
@@ -27,7 +27,12 @@ public class RoomRepository implements JdbcRepository<Room> {
                 "(date_format(?, '%Y-%m-%d') not between date_format(check_in, '%Y-%m-%d') and date_format(check_out, '%Y-%m-%d') and " +
                 "date_format(?, '%Y-%m-%d') not between date_format(check_in, '%Y-%m-%d') and date_format(check_out, '%Y-%m-%d')) or " +
                 "(check_in is null and check_out is null)";
-        return jdbcTemplate.query(sql, roomRowMapper(), minPrice, maxPrice, numberOfPeople, checkIn, checkOut);
+
+        List<Room> rooms = jdbcTemplate.query(sql, roomRowMapper(), minPrice, maxPrice, numberOfPeople, checkIn, checkOut);
+
+        return rooms.stream()
+                .map(room -> addThumbnailsAndOptionAndBadges(room))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -35,8 +40,11 @@ public class RoomRepository implements JdbcRepository<Room> {
         String sql = "select `id`, `max`, `name`, `rating`, `latitude`, `longitude`, `bedroom_count`, `bed_count`, " +
                 "`bathroom_count`, `address`, `detail_address`, `comment_count`, `original_price`, `sale_price`, " +
                 "`flexible_refund`, `immediate_booking` from room where id = ?";
-        List<Room> result = jdbcTemplate.query(sql, roomRowMapper(), id);
-        return result.stream().findAny();
+
+        List<Room> rooms = jdbcTemplate.query(sql, roomRowMapper(), id);
+
+        return rooms.stream()
+                .map(room -> addThumbnailsAndOptionAndBadges(room)).findAny();
     }
 
     @Override
@@ -44,22 +52,40 @@ public class RoomRepository implements JdbcRepository<Room> {
         String sql = "select `id`, `max`, `name`, `rating`, `latitude`, `longitude`, `bedroom_count`, `bed_count`, " +
                 "`bathroom_count`, `address`, `detail_address`, `comment_count`, `original_price`, `sale_price`, " +
                 "`flexible_refund`, `immediate_booking` from room";
-        return jdbcTemplate.query(sql, roomRowMapper());
+
+        List<Room> rooms = jdbcTemplate.query(sql, roomRowMapper());
+
+        return rooms.stream()
+                .map(room -> addThumbnailsAndOptionAndBadges(room))
+                .collect(Collectors.toList());
     }
 
-    @Override
-    public void insert(Room room) {
+    private Room addThumbnailsAndOptionAndBadges(Room room) {
+        List<Thumbnail> thumbnails = findThumbnailsByRoomId(room.getId());
+        room.setThumbnails(thumbnails);
 
+        Optional<Option> option = findOptionByRoomId(room.getId());
+        room.setOption(option);
+
+        List<Badge> badges = findBadgeByRoomId(room.getId());
+        room.setBadges(badges);
+
+        return room;
     }
 
-    @Override
-    public void update(Room room, Long id) {
-
+    private List<Thumbnail> findThumbnailsByRoomId(Long roomId) {
+        String sql = "select room_id, thumbnail from thumbnail where room_id = ?";
+        return jdbcTemplate.query(sql, thumbnailRowMapper(), roomId);
     }
 
-    @Override
-    public void delete(Long id) {
+    private Optional<Option> findOptionByRoomId(Long roomId) {
+        String sql = "select room_id, kitchen, air_conditioner, wifi, free_parking_lot, hair_dryer from `option` where room_id = ?";
+        return jdbcTemplate.query(sql, optionRowMapper(), roomId).stream().findAny();
+    }
 
+    private List<Badge> findBadgeByRoomId(Long roomId) {
+        String sql = "select room_id, type from badge where room_id = ?";
+        return jdbcTemplate.query(sql, badgeRowMapper(), roomId);
     }
 
     private RowMapper<Room> roomRowMapper() {
@@ -78,10 +104,35 @@ public class RoomRepository implements JdbcRepository<Room> {
                     .setCommentCount(resultSet.getInt("comment_count"))
                     .setOriginalPrice(resultSet.getInt("original_price"))
                     .setSalePrice(resultSet.getInt("sale_price"))
-                    .setFlexibleRefund(resultSet.getInt("flexible_refund"))
-                    .setImmediateBooking(resultSet.getInt("immediate_booking"))
+                    .setFlexibleRefund(resultSet.getBoolean("flexible_refund"))
+                    .setImmediateBooking(resultSet.getBoolean("immediate_booking"))
+                    .setThumbnails(new ArrayList<>())
+                    .setOptions(Optional.empty())
+                    .setBadges(new ArrayList<>())
                     .build();
             return room;
+        };
+    }
+
+    private RowMapper<Thumbnail> thumbnailRowMapper() {
+        return (resultSet, rowNum) -> {
+            Thumbnail thumbnail = new Thumbnail(resultSet.getString("thumbnail"));
+            return thumbnail;
+        };
+    }
+
+    private RowMapper<Badge> badgeRowMapper() {
+        return (resultSet, rowNum) -> {
+            Badge badge = new Badge(resultSet.getString("type"));
+            return badge;
+        };
+    }
+
+    private RowMapper<Option> optionRowMapper() {
+        return (resultSet, rowNum) -> {
+            Option option = new Option(resultSet.getBoolean("kitchen"), resultSet.getBoolean("air_conditioner"),
+                    resultSet.getBoolean("wifi"), resultSet.getBoolean("free_parking_lot"), resultSet.getBoolean("hair_dryer"));
+            return option;
         };
     }
 }
