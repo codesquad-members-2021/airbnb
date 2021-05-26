87bcc1a2483c8dd90c5fbdd079d455e9d52c74e2
Shion <ehdrhelr@gmail.com>
2021-05-26 11:33:41 +0900

    [#5] refactor : api url 및 메소드명 변경

--- BE/airbnb/src/main/java/team01/airbnb/controller/AccommodationController.java
+++ BE/airbnb/src/main/java/team01/airbnb/controller/AccommodationController.java
@@ -33,8 +33,8 @@ public class AccommodationController {
         return ApiResult.succeed(accommodationService.findAvailableAccommodationsForReservation());
     }
 
-    @PostMapping("/save")
-    public ApiResult save(AccommodationSaveRequestDto accommodationSaveRequestDto) {
+    @PostMapping("/")
+    public ApiResult createAccommodation(AccommodationSaveRequestDto accommodationSaveRequestDto) {
         accommodationService.save(accommodationSaveRequestDto);
         return ApiResult.ok();
     }
