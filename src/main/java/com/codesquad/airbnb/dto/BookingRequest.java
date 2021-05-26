c907c96a3da82662a3a70147961c9c864b1e45f6
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 00:46:24 +0900

    add : add BookingRequest as request DTO

---
+++ src/main/java/com/codesquad/airbnb/dto/BookingRequest.java
@@ -0,0 +1,50 @@
+package com.codesquad.airbnb.dto;
+
+import com.codesquad.airbnb.domain.Booking;
+
+public class BookingRequest {
+
+    private Long userId;
+    private Long roomId;
+    private String checkIn;
+    private String checkOut;
+    private int numberOfPeople;
+    private int totalPrice;
+
+    public BookingRequest(Long userId, Long roomId, String checkIn, String checkOut, int numberOfPeople, int totalPrice) {
+        this.userId = userId;
+        this.roomId = roomId;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.numberOfPeople = numberOfPeople;
+        this.totalPrice = totalPrice;
+    }
+
+    public Booking toBooking() {
+        return new Booking(userId, roomId, checkIn, checkOut, numberOfPeople, totalPrice);
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+}
