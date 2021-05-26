d2725cf47cd4fec12ff5c9f0548b7035099c1c0f
junamee <skawn11114@gmail.com>
2021-05-27 02:55:59 +0900

    Feat.useAxios 커스텀훅으로 구현, 타입스크립트 적용

--- frontend/src/customHook/axiosAPI.tsx
+++ frontend/src/customHook/axiosAPI.tsx
@@ -1,1 +1,9 @@
-export const url = `http://13.125.140.183/search/prices`
+import axios from 'axios'
+async function getData() {
+  const response = await axios.get(
+    `http://13.125.140.183/search/prices?check-in=2021-03-02&check-out=2021-03-04`
+  )
+  return response
+}
+
+export default getData
