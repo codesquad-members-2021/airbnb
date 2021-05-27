21c44374fd1980e870e7bc07f181b2584e6c1bfd
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-05-27 18:10:42 +0900

    feat: WishList 조회를 위한 Controller 추가

---
+++ BE/src/main/java/com/codesquad/airbnb/controller/WishListController.java
@@ -0,0 +1,22 @@
+package com.codesquad.airbnb.controller;
+
+import com.codesquad.airbnb.dto.PropertiesResponseDTO;
+import com.codesquad.airbnb.service.PropertyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WishListController {
+
+    PropertyService propertyService;
+
+    public WishListController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    @GetMapping("/wishlist")
+    public ResponseEntity<PropertiesResponseDTO> wishlist() {
+        return ResponseEntity.ok().body(propertyService.findByWishList());
+    }
+}
