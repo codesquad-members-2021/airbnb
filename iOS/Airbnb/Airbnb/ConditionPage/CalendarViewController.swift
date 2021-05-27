7cfd65beff9d8a29519eb0ac8b5af071043936ea
Lia <judykim257@gmail.com>
2021-05-27 11:53:12 +0900

    feat: ✨Calendar View Layout(#30)

--- iOS/Airbnb/Airbnb/ConditionPage/CalendarViewController.swift
+++ iOS/Airbnb/Airbnb/ConditionPage/CalendarViewController.swift
@@ -12,55 +12,52 @@ class CalendarViewController: UIViewController {
     static let headerElementKind = "header-element-kind"
     
     private var collectionView: UICollectionView!
-    private var dataSource: UICollectionViewDiffableDataSource<Int, City>!
-    
-    private var viewModel = MainPageUseCase()
-
+    private var dataSource: UICollectionViewDiffableDataSource<Date, Day>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureSearchBar()
-        viewModel.requestMainPage()
         configureHierarchy()
         configureDataSource()
-        applySnapshots(with: mainPage.cities)
+        applySnapshots()
     }
     
 }
-
-
 
 //MARK:- Layout & DataSource
 
 extension CalendarViewController {
     
     private func configureHierarchy() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout(isHeaderExist: true))
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
         collectionView.delegate = self
+        collectionView.allowsMultipleSelection = true
         view.addSubview(collectionView)
     }
     
-    private func createLayout(isHeaderExist: Bool) -> UICollectionViewLayout {
+    private func createLayout() -> UICollectionViewLayout {
         let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
             
             // section
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            let trailingGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(
+                                                                   widthDimension: .fractionalWidth(1),
+                                                                   heightDimension: .fractionalWidth(1/7)),
+                                                                   subitem: item, count: 7)
+            trailingGroup.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 0, bottom: 0, trailing: 0)
+            let containerGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .fractionalHeight(0.4)),
+                                                  subitems: [trailingGroup])
+            let section = NSCollectionLayoutSection(group: containerGroup)
             
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.2))
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-            
-            let section = NSCollectionLayoutSection(group: group)
             section.interGroupSpacing = 10
-            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 20, trailing: 10)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10)
             
             // header
-            if isHeaderExist {
-                self.createSectionHeaderLayout(with: section)
-            }
+            self.createSectionHeaderLayout(with: section)
             return section
         }
         return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
@@ -70,40 +67,30 @@ extension CalendarViewController {
         let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
             layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                heightDimension: .estimated(44)),
-            elementKind: SearchCityViewController.headerElementKind,
+            elementKind: CalendarViewController.headerElementKind,
             alignment: .top)
         section.boundarySupplementaryItems = [sectionHeader]
     }
     
     private func configureDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Int, City>(collectionView: collectionView) {
-            (collectionView, indexPath, city) -> UICollectionViewCell? in
-            self.collectionView.register(CityCell.nib, forCellWithReuseIdentifier: CityCell.reuseIdentifier)
-            
-            let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: CityCell.reuseIdentifier, for: indexPath) as! CityCell
-            cell.fillUI(with: city)
-            return cell
+        
+        let listCellRegistration = UICollectionView.CellRegistration<DayCell, Day> { (cell, indexPath, day) in
+            cell.fillInfo(with: day.date)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Date, Day>(collectionView: collectionView) {
+            (collectionView, indexPath, date) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: date)
         }
         self.configureSupplementaryView()
     }
     
-    private func configureRegionDataSource() {
-        regionDataSource = UICollectionViewDiffableDataSource<Int, Region>(collectionView: collectionView) {
-            (collectionView, indexPath, city) -> UICollectionViewCell? in
-            self.collectionView.register(RegionCell.nib, forCellWithReuseIdentifier: RegionCell.reuseIdentifier)
-            
-            let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: RegionCell.reuseIdentifier, for: indexPath) as! RegionCell
-            cell.fillUI(with: city)
-            return cell
-        }
-        configureEmptySupplementaryView()
-    }
-    
     private func configureSupplementaryView() {
         let supplementaryRegistration = UICollectionView.SupplementaryRegistration
-        <TitleSupplementaryView>(elementKind: SearchCityViewController.headerElementKind) {
+        <TitleSupplementaryView>(elementKind: CalendarViewController.headerElementKind) {
             (supplementaryView, string, indexPath) in
-            supplementaryView.label.text = "근처의 인기 여행지"
+            let date = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+            supplementaryView.label.text = "\(date.year)년 \(date.month)월"
             supplementaryView.label.font = UIFont.preferredFont(forTextStyle: .title2)
         }
         dataSource.supplementaryViewProvider = { (view, kind, index) in
@@ -111,43 +98,47 @@ extension CalendarViewController {
                 using: supplementaryRegistration, for: index)
         }
     }
- 
-    private func configureEmptySupplementaryView() {
-        let supplementaryRegistration = UICollectionView.SupplementaryRegistration
-        <UICollectionReusableView>(elementKind: SearchCityViewController.headerElementKind) {
-            (supplementaryView, string, indexPath) in
-        }
-        regionDataSource.supplementaryViewProvider = { (view, kind, index) in
-            return self.collectionView.dequeueConfiguredReusableSupplementary(
-                using: supplementaryRegistration, for: index)
-        }
-    }
     
-    private func applySnapshots(with cities: [City]) {
-        var snapshot = NSDiffableDataSourceSnapshot<Int, City>()
-        
-        snapshot.appendSections([1])
-        snapshot.appendItems(cities)
+    private func applySnapshots() {
+        var snapshot = NSDiffableDataSourceSnapshot<Date, Day>()
+
+        let thisMonth = Date().month
+        Array(thisMonth...thisMonth+10).forEach {
+            let month = Calendar.current.date(from: DateComponents(year: 2021, month: $0, day: 1)) ?? Date()
+            snapshot.appendSections([month])
+            snapshot.appendItems(DateGenerator.getTheMonth(year: 2021, month: $0))
+        }
         self.dataSource.apply(snapshot)
-    }
-    
-    private func applyRegionSnapshots() {
-        var snapshot = NSDiffableDataSourceSnapshot<Int, Region>()
-        
-        snapshot.appendSections([1])
-        snapshot.appendItems(Region.allCities)
-        self.regionDataSource.apply(snapshot)
     }
     
 }
 
 //MARK:- Selection
 
-extension SearchCityViewController: UICollectionViewDelegate {
+extension CalendarViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? DayCell else { return }
+        guard let selectedIndexs = collectionView.indexPathsForSelectedItems else { return }
+        guard let date = cell.date?.description else { return }
+        print(date)
+        
+        switch selectedIndexs.count {
+        case 0:
+            break
+        case 1:
+            cell.selectedType = .one
+        case 2:
+            let minCell = collectionView.cellForItem(at: selectedIndexs.min()!) as? DayCell ?? DayCell()
+            let maxCell = collectionView.cellForItem(at: selectedIndexs.max()!) as? DayCell ?? DayCell()
+            minCell.selectedType = .min
+            maxCell.selectedType = .max
+        default:
+            selectedIndexs.forEach { index in
+                collectionView.deselectItem(at: index, animated: false)
+            }
+        }
     }
     
 }
-
 
