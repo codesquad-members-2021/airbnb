3bcd2805451fecc5724336991d489547210f0db1
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-27 14:03:59 +0900

    add : add method

--- src/main/java/com/codesquad/airbnb/controller/BookingController.java
+++ src/main/java/com/codesquad/airbnb/controller/BookingController.java
@@ -1,13 +1,31 @@
 package com.codesquad.airbnb.controller;
 
+import com.codesquad.airbnb.domain.Booking;
+import com.codesquad.airbnb.dto.BookingRequest;
+import com.codesquad.airbnb.repository.BookingRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/api/booking")
 public class BookingController {
 
+    private final BookingRepository bookingRepository;
 
+    public BookingController(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
 
+    @PostMapping
+    public void booking(BookingRequest bookingRequest) {
+        Booking booking = bookingRequest.toBooking();
 
+    }
+
+    @DeleteMapping
+    public void cancelBooking() {
+
+    }
 }
