a6ed1042142288de204a9d8daeb9383834781b6a
Lia <judykim257@gmail.com>
2021-05-27 11:54:06 +0900

    feat: ✨Calendar Date Logic(#30)

--- iOS/Airbnb/Airbnb/ConditionPage/Date+extension.swift
+++ iOS/Airbnb/Airbnb/ConditionPage/Date+extension.swift
@@ -2,7 +2,31 @@
 //  Date+extension.swift
 //  Airbnb
 //
-//  Created by 김지경 on 2021/05/27.
+//  Created by Lia on 2021/05/27.
 //
 
 import Foundation
+
+extension Date {
+    var year: Int {
+        let calendar = Calendar.current
+        return calendar.component(.year, from: self)
+    }
+    var month: Int {
+        let calendar = Calendar.current
+        return calendar.component(.month, from: self)
+    }
+    var weekday: Int {
+        let calendar = Calendar.current
+        return calendar.component(.weekday, from: self)
+    }
+    var day: Int {
+        let calendar = Calendar.current
+        return calendar.component(.day, from: self)
+    }
+    var description: String {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyyy-MM-dd"
+        return dateFormat.string(for: self) ?? ""
+    }
+}
