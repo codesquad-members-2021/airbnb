2731347f05668e7e1702fd4874f249c664874ca8
eve712 <62237639+eve712@users.noreply.github.com>
2021-05-28 02:10:35 +0900

    feat: 체크인, 체크아웃 날짜 상태로 받아서 searchBar에 렌더링

--- FE/airbnb/src/component/searchBar/Period.tsx
+++ FE/airbnb/src/component/searchBar/Period.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { checkinDateState, checkoutDateState } from "state/atoms/calendarAtoms";
 
 interface Props {
   onClick: (e: React.MouseEvent) => void;
 }
 
 function Period({ onClick }: Props) {
+  const checkinDate = useRecoilValue(checkinDateState);
+  const checkoutDate = useRecoilValue(checkoutDateState);
+  const checkinText: string =
+    checkinDate !== "날짜 입력" ? `${checkinDate.substr(4, 2)}월 ${checkinDate.substr(6, 2)}일` : checkinDate;
+  const checkoutText: string =
+    checkoutDate !== "날짜 입력" ? `${checkoutDate.substr(4, 2)}월 ${checkoutDate.substr(6, 2)}일` : checkoutDate;
   return (
     <>
       <CheckInContainer onClick={onClick}>
         <Title>체크인</Title>
-        <Content>날짜 입력</Content>
+        <Content>{checkinText}</Content>
       </CheckInContainer>
       <CheckOutContainer onClick={onClick}>
         <Title>체크아웃</Title>
-        <Content>날짜 입력</Content>
+        <Content>{checkoutText}</Content>
       </CheckOutContainer>
     </>
   );
