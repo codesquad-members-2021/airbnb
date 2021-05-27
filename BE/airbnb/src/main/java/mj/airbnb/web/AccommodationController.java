de83c5f011d89014017956c73a5ac1660259e9d8
MJbae <gently.gogo@gmail.com>
2021-05-27 23:54:50 +0900

    refactor: final 제어자 추가

--- BE/airbnb/src/main/java/mj/airbnb/web/AccommodationController.java
+++ BE/airbnb/src/main/java/mj/airbnb/web/AccommodationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("/api/accommodations")
 public class AccommodationController {
 
-    private AccommodationService accommodationService;
+    private final AccommodationService accommodationService;
 
     public AccommodationController(AccommodationService accommodationService) {
         this.accommodationService = accommodationService;
--- BE/airbnb/src/main/java/mj/airbnb/web/dto/SearchRequestDto.java
+++ BE/airbnb/src/main/java/mj/airbnb/web/dto/SearchRequestDto.java
@@ -4,7 +4,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Optional;
 
 public class SearchRequestDto {
 
