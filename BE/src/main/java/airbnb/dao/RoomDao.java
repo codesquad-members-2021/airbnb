0b544cda779e93fbac19d26f6dec618c595e2c5d
95degree <95degree@naver.com>
2021-05-27 15:03:51 +0900

    feat: ✨유저에 검색에 따른 숙소 조회 API 생성(#21)
    - 가격 범위
    - 최대 인원
    - 도시
    - 체크인 체크아웃

--- BE/src/main/java/airbnb/dao/RoomDao.java
+++ BE/src/main/java/airbnb/dao/RoomDao.java
@@ -1,6 +1,7 @@
 package airbnb.dao;
 
 import airbnb.domain.City;
+import airbnb.domain.Cost;
 import airbnb.domain.Room;
 import airbnb.domain.Schedule;
 import airbnb.mapper.ImageMapper;
@@ -20,8 +21,9 @@ import java.util.stream.Collectors;
 public class RoomDao {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
-    private final RoomMapper roomMapper = new RoomMapper();
     private final ImageDao imageDao;
+    private final RoomMapper roomMapper = new RoomMapper();
+    private final MapSqlParameterSource parameter = new MapSqlParameterSource();
 
     public RoomDao(NamedParameterJdbcTemplate jdbcTemplate, ImageDao imageDao) {
         this.jdbcTemplate = jdbcTemplate;
@@ -41,10 +43,33 @@ public class RoomDao {
         LocalDate chekIn = schedule.getCheckIn();
         LocalDate chekOut = schedule.getCheckOut();
 
-        MapSqlParameterSource parameter = new MapSqlParameterSource();
+
         parameter.addValue("cityId", cityId);
         parameter.addValue("checkIn", chekIn);
         parameter.addValue("checkOut", chekOut);
+
+        List<Room> rooms = jdbcTemplate.query(sql, parameter, roomMapper);
+        rooms.forEach(room -> room.setImages(imageDao.findByRoomId(room.getId())));
+        return rooms;
+    }
+
+    public List<Room> findSearchRooms(Long cityId, Schedule schedule, Cost cost, int maxPeopleCount){
+        String sql = "SELECT a.id, price, title, description, people, oneroom, bed, bath, hair_dryer, air_conditioner, wifi, clean_tax FROM room AS a left join reservation AS b ON a.id = b.room_id " +
+                "WHERE (b.id IS NULL OR ((b.check_in NOT BETWEEN :checkIn AND :checkOut) AND (b.check_out NOT BETWEEN :checkIn AND :checkOut))) " +
+                "AND (a.price between :minCost AND :maxCost) " +
+                "AND (a.people >= :maxPeopleCount";
+
+        LocalDate chekIn = schedule.getCheckIn();
+        LocalDate chekOut = schedule.getCheckOut();
+        int minCost = cost.getMinCost();
+        int maxCost = cost.getMaxCost();
+
+        parameter.addValue("cityId", cityId);
+        parameter.addValue("checkIn", chekIn);
+        parameter.addValue("checkOut", chekOut);
+        parameter.addValue("minCost", minCost);
+        parameter.addValue("maxCost", maxCost);
+        parameter.addValue("maxPeopleCount", maxPeopleCount);
 
         List<Room> rooms = jdbcTemplate.query(sql, parameter, roomMapper);
         rooms.forEach(room -> room.setImages(imageDao.findByRoomId(room.getId())));
