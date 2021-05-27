d5c6e817f972d2a026cb8a6afc885203bd47657d
Hoyoung Son <xzxking17@gmail.com>
2021-05-28 01:27:31 +0900

    add: SearchBar의 모든 Modal 생성 (기본 틀) [#24]

---
+++ FE/src/components/Main/TopBackground/Modals/CalendarModal.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import Modal from '../../../Common/Modal';
+
+const CalendarModal = () => {
+  return (
+    <CalendarModalLayout>
+    </CalendarModalLayout>
+  );
+};
+
+export default CalendarModal;
+
+// --- Styled Components ---
+const CalendarModalLayout = styled(Modal)`
+`;
---
+++ FE/src/components/Main/TopBackground/Modals/FeeModal.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import Modal from '../../../Common/Modal';
+
+const FeeModal = () => {
+  return (
+    <FeeModalLayout>
+    </FeeModalLayout>
+  );
+};
+
+export default FeeModal;
+
+// --- Styled Components ---
+const FeeModalLayout = styled(Modal)`
+`;
