7fd84211285b17efd1c72a48637adf63dd754f5c
Ray suh <readingod@naver.com>
2021-05-27 11:48:38 +0900

    feat: Add Observable and Bind Method (#18)

--- Airbnb/Airbnb/Presenter/Controller/PriceViewController.swift
+++ Airbnb/Airbnb/Presenter/Controller/PriceViewController.swift
@@ -1,14 +1,41 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class PriceViewController: UIViewController {
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     
     private let viewModel = PriceViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
     }
     
     func setupInfo(of location:String, of date:String) {
         viewModel.setupInfo(of: location, of: date)
+    }
+}
+
+private extension PriceViewController {
+    
+    private func bind() {
+        bindLocationLabel()
+        bindDateLabel()
+    }
+    
+    private func bindLocationLabel() {
+        viewModel.locationData
+            .drive(locationLabel.rx.text)
+            .disposed(by: rx.disposeBag)
+    }
+    
+    private func bindDateLabel() {
+        viewModel.dateData
+            .drive(dateLabel.rx.text)
+            .disposed(by: rx.disposeBag)
     }
 }
--- Airbnb/Airbnb/Presenter/ViewModel/PriceViewModel.swift
+++ Airbnb/Airbnb/Presenter/ViewModel/PriceViewModel.swift
@@ -7,6 +7,14 @@ class PriceViewModel {
     private var locationInfo: String?
     private var dateInfo: String?
     
+    lazy var locationData:Driver<String> = {
+        return Observable.just(locationInfo ?? "").asDriver(onErrorJustReturn: "")
+    }()
+    
+    lazy var dateData:Driver<String> = {
+        return Observable.just(dateInfo ?? "").asDriver(onErrorJustReturn: "")
+    }()
+    
     func setupInfo(of location:String, of date:String) {
         locationInfo = location
         dateInfo = date
