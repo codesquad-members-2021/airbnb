f6649b1e75ef7cbd7dcd98ebadad5ee3263255c7
eve712 <62237639+eve712@users.noreply.github.com>
2021-05-28 02:20:55 +0900

    feat: 날짜 선택 기능, 일자 mouseOver 이벤트 작성
    * 날짜를 선택하면 searchBar에 날짜가 반영되고, 달력에 검은 원으로 표시된다.
    * 체크인만 선택됐을 때, 체크인 이후 날짜에 mouseOver되면 사이 기간 일자에 회색 박스가 생긴다.

--- FE/airbnb/src/component/searchBar/calendar/Calendar.tsx
+++ FE/airbnb/src/component/searchBar/calendar/Calendar.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import styled from "styled-components";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { checkinDateState, checkoutDateState, hoverDateState } from "state/atoms/calendarAtoms";
+import Day from "component/searchBar/calendar/Day";
 import getCalendarData from "component/searchBar/calendar/getCalendarData";
-
-const days: string[] = ["일", "월", "화", "수", "목", "금", "토"];
 
 interface Props {
   year: number;
   month: number;
+  today: string;
 }
 
-function Calendar({ year, month }: Props) {
+function Calendar({ year, month, today }: Props) {
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const [checkinDate, setCheckinDate] = useRecoilState(checkinDateState);
+  const [checkoutDate, setCheckoutDate] = useRecoilState(checkoutDateState);
+  const setHoverDate = useSetRecoilState(hoverDateState);
+
   if (month < 0) {
     month += 12;
     year--;
@@ -19,12 +25,42 @@ function Calendar({ year, month }: Props) {
     year++;
   }
   const weeksData = getCalendarData(year, month);
+  const didntSelect = "날짜 입력";
+
+  const handleClickDate = (e: React.MouseEvent<HTMLTableElement>): void => {
+    const target = e.target as HTMLElement;
+    const div: HTMLDivElement | null = target.closest(".dayDiv");
+    if (!div || !div.dataset.date) return;
+    if (+div.dataset.date < +today) return;
+
+    const selectedDate: string = div.dataset.date;
+    if (checkinDate === didntSelect) setCheckinDate(selectedDate);
+    else if (+selectedDate < +checkinDate) {
+      setCheckinDate(selectedDate);
+      setCheckoutDate(didntSelect);
+      setHoverDate(selectedDate);
+    } else {
+      setCheckoutDate(selectedDate);
+      setHoverDate(selectedDate);
+    }
+  };
+
+  const handleMouseOverDate = (e: React.MouseEvent<HTMLTableElement>): void => {
+    if (checkinDate === didntSelect || checkoutDate !== didntSelect) return;
+    const target = e.target as HTMLElement;
+    const div: HTMLDivElement | null = target.closest(".dayDiv");
+    if (!div || !div.dataset.date) return;
+
+    const enteredDate: string = div.dataset.date;
+    setHoverDate(enteredDate);
+  };
+
   return (
     <CalendarContainer>
       <CalendarTitle>
         {year}년 {month + 1}월
       </CalendarTitle>
-      <CalendarTable>
+      <CalendarTable onClick={handleClickDate} onMouseOver={handleMouseOverDate}>
         <thead>
           <tr>
             {days.map((day) => (
@@ -33,10 +69,10 @@ function Calendar({ year, month }: Props) {
           </tr>
         </thead>
         <tbody>
-          {weeksData.map((week) => (
-            <tr>
-              {week.map((day) => (
-                <td>{day}</td>
+          {weeksData.map((week, i) => (
+            <tr key={year + month + i}>
+              {week.map((day, i) => (
+                <Day key={day + i} day={day} year={year} month={month} today={today} />
               ))}
             </tr>
           ))}
@@ -65,11 +101,5 @@ const CalendarTable = styled.table`
     font-size: 12px;
     color: ${({ theme }) => theme.color.gray3};
     line-height: 24px;
-  }
-  td {
-    width: 48px;
-    height: 48px;
-    text-align: center;
-    line-height: 48px;
   }
 `;
