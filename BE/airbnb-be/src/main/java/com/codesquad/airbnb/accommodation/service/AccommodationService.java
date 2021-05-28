dafdbcaca23f2a3032667fe693e2e796b880b125
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-29 00:38:01 +0900

    feat: 목업과 도메인 개발 환경 분리

--- BE/airbnb-be/src/main/java/com/codesquad/airbnb/accommodation/service/AccommodationService.java
+++ BE/airbnb-be/src/main/java/com/codesquad/airbnb/accommodation/service/AccommodationService.java
@@ -2,29 +2,9 @@ package com.codesquad.airbnb.accommodation.service;
 
 import com.codesquad.airbnb.accommodation.controller.AccommodationRequestDTO;
 import com.codesquad.airbnb.accommodation.controller.AccommodationResponseDTO;
-import com.codesquad.airbnb.common.utils.DummyDataFactory;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class AccommodationService {
-    public List<AccommodationResponseDTO> readAll(AccommodationRequestDTO accommodationRequestDTO) {
-        List<AccommodationResponseDTO> result = DummyDataFactory.accommodationResponseDTOsWithId();
-
-        if (accommodationRequestDTO.getStartPrice() != null) {
-            result = result.stream()
-                             .filter(accommodationResponseDTO -> accommodationRequestDTO.getStartPrice() <= accommodationResponseDTO.pricePerNight())
-                             .collect(Collectors.toList());
-        }
-
-        if (accommodationRequestDTO.getEndPrice() != null) {
-            result = result.stream()
-                             .filter(accommodationResponseDTO -> accommodationResponseDTO.pricePerNight() <= accommodationRequestDTO.getEndPrice())
-                             .collect(Collectors.toList());
-        }
-
-        return result;
-    }
+public interface AccommodationService {
+    List<AccommodationResponseDTO> readAll(AccommodationRequestDTO accommodationRequestDTO);
 }
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar BE/airbnb-be/build/libs/*.jar
+web: java -Dspring.profiles.active=mockup -Dserver.port=$PORT $JAVA_OPTS -jar BE/airbnb-be/build/libs/*.jar
