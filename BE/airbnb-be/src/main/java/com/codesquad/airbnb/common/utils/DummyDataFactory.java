db228fe0300c44c39427fb0605620e72d1b22346
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-05-29 00:47:34 +0900

    refactor: 더미데이터 메소드 구조 변경

--- BE/airbnb-be/src/main/java/com/codesquad/airbnb/common/utils/DummyDataFactory.java
+++ BE/airbnb-be/src/main/java/com/codesquad/airbnb/common/utils/DummyDataFactory.java
@@ -1,10 +1,7 @@
 package com.codesquad.airbnb.common.utils;
 
 import com.codesquad.airbnb.accommodation.controller.*;
-import com.codesquad.airbnb.accommodation.domain.Accommodation;
-import com.codesquad.airbnb.accommodation.domain.AccommodationOption;
-import com.codesquad.airbnb.accommodation.domain.AccommodationType;
-import com.codesquad.airbnb.accommodation.domain.RestroomType;
+import com.codesquad.airbnb.accommodation.domain.*;
 import com.codesquad.airbnb.reservation.controller.ReservationDTO;
 import com.codesquad.airbnb.wishlist.WishlistItemDTO;
 
@@ -65,7 +62,7 @@ public class DummyDataFactory {
         return new AccommodationHost(1L, "name", "https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg");
     }
 
-    public static Accommodation accommodationTypeSuiteroom() {
+    public static AccommodationBuilder accommodationBuilderTypeSuiteroom() {
         return Accommodation.builder()
                        .name("어텀호텔 스위트룸")
                        .accommodationOption(accommodationOptionOfNormal())
@@ -74,11 +71,10 @@ public class DummyDataFactory {
                        .mainImage("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg")
                        .images(Arrays.asList("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg", "https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg"))
                        .description("어텀호텔 스위트룸입니다.")
-                       .accommodationHost(accommodationHost())
-                       .build();
+                       .accommodationHost(accommodationHost());
     }
 
-    public static Accommodation accommodationTypeOneroom() {
+    public static AccommodationBuilder accommodationBuilderTypeOneroom() {
         return Accommodation.builder()
                        .name("롤로호텔 일반객실")
                        .accommodationOption(accommodationOptionOfOneroom())
@@ -87,11 +83,10 @@ public class DummyDataFactory {
                        .mainImage("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg")
                        .images(Arrays.asList("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg", "https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg"))
                        .description("롤로호텔 일반객실입니다.")
-                       .accommodationHost(accommodationHost())
-                       .build();
+                       .accommodationHost(accommodationHost());
     }
 
-    public static Accommodation accommodationTypeLowQuality() {
+    public static AccommodationBuilder accommodationBuilderTypeLowQuality() {
         return Accommodation.builder()
                        .name("프레디 여관")
                        .accommodationOption(accommodationOptionOfPublicRestroom())
@@ -100,15 +95,22 @@ public class DummyDataFactory {
                        .mainImage("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg")
                        .images(Arrays.asList("https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg", "https://image.zdnet.co.kr/2016/12/08/imc_47ix3fAqITYz5QtR.jpg"))
                        .description("프레디 여관입니다.")
-                       .accommodationHost(accommodationHost())
-                       .build();
+                       .accommodationHost(accommodationHost());
     }
 
     public static List<Accommodation> accommodations() {
         return Arrays.asList(
-                DummyDataFactory.accommodationTypeSuiteroom(),
-                DummyDataFactory.accommodationTypeOneroom(),
-                DummyDataFactory.accommodationTypeLowQuality()
+                DummyDataFactory.accommodationBuilderTypeSuiteroom().build(),
+                DummyDataFactory.accommodationBuilderTypeOneroom().build(),
+                DummyDataFactory.accommodationBuilderTypeLowQuality().build()
+        );
+    }
+
+    public static List<Accommodation> accommodationsWithId() {
+        return Arrays.asList(
+                DummyDataFactory.accommodationBuilderTypeSuiteroom().id(1L).build(),
+                DummyDataFactory.accommodationBuilderTypeOneroom().id(2L).build(),
+                DummyDataFactory.accommodationBuilderTypeLowQuality().id(3L).build()
         );
     }
 
@@ -119,25 +121,15 @@ public class DummyDataFactory {
     }
 
     public static List<AccommodationResponse> accommodationResponseDTOsWithId() {
-        List<AccommodationResponse> accommodationResponses = accommodationResponseDTOs();
-
-        for (int i = 0; i < accommodationResponses.size(); i++) {
-            accommodationResponses.get(i).setId(Long.valueOf(i + 1));
-        }
-
-        return accommodationResponses;
+        return accommodationsWithId().stream()
+                       .map(AccommodationResponse::from)
+                       .collect(Collectors.toList());
     }
 
     public static List<AccommodationDTO> accommodationDTOsWithId() {
-        List<AccommodationDTO> accommodationDTO = accommodations().stream()
-                                                          .map(AccommodationDTO::from)
-                                                          .collect(Collectors.toList());
-
-        for (int i = 0; i < accommodationDTO.size(); i++) {
-            accommodationDTO.get(i).setId(Long.valueOf(i + 1));
-        }
-
-        return accommodationDTO;
+        return accommodationsWithId().stream()
+                       .map(AccommodationDTO::from)
+                       .collect(Collectors.toList());
     }
 
     public static List<AccommodationPriceStats> accommodationPriceStats() {
@@ -167,7 +159,7 @@ public class DummyDataFactory {
     }
 
     public static ReservationDTO reservation() {
-        return new ReservationDTO(1L, AccommodationResponse.from(accommodationTypeSuiteroom()));
+        return new ReservationDTO(1L, AccommodationResponse.from(accommodationBuilderTypeSuiteroom().build()));
     }
 
     public static List<WishlistItemDTO> wishlist() {
@@ -175,6 +167,6 @@ public class DummyDataFactory {
     }
 
     public static WishlistItemDTO wishlistItem() {
-        return new WishlistItemDTO(1L, AccommodationResponse.from(accommodationTypeSuiteroom()));
+        return new WishlistItemDTO(1L, AccommodationResponse.from(accommodationBuilderTypeSuiteroom().build()));
     }
 }
