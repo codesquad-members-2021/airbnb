b334585266da39003c8eeaaff480fe4692d1d22c
Tree <gmldbs1109@naver.com>
2021-05-28 12:08:42 +0900

    chore : wishlist를 리스트에 add하는 기능 분리(#12)

--- be/airbnb/src/main/java/com/team19/airbnb/service/WishlistService.java
+++ be/airbnb/src/main/java/com/team19/airbnb/service/WishlistService.java
@@ -3,12 +3,9 @@ package com.team19.airbnb.service;
 import com.team19.airbnb.domain.User;
 import com.team19.airbnb.domain.Wishlist;
 import com.team19.airbnb.dto.WishListRequestDTO;
-import com.team19.airbnb.repository.UserDAO;
 import com.team19.airbnb.repository.WishlistDAO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class WishlistService {
@@ -24,9 +21,8 @@ public class WishlistService {
     @Transactional
     public void addWishList(WishListRequestDTO wishListRequestDTO, Long userId) {
         User user = userService.findUser(userId);
-        List<Wishlist> wishlists = user.getWishlists();
-        wishlists.add(wishListRequestDTO.toEntity());
         Wishlist wishlist = wishListRequestDTO.toEntity();
+        user.addWishlist(wishlist);
         wishlistDAO.updateWishlist(user, wishlist);
     }
 }
