da447c32e2b09b30e4f4c9c52bbae30892221720
Jisoo Kim <ghis22130@naver.com>
2021-05-29 15:58:09 +0900

    feat: [#10] 이미지 로드 실패 시 처리

--- iOS/Arirbnb/Arirbnb/SupportingFiles/UIImageView+Extension.swift
+++ iOS/Arirbnb/Arirbnb/SupportingFiles/UIImageView+Extension.swift
@@ -9,8 +9,15 @@ import UIKit
 import Kingfisher
 
 extension UIImageView {
-    func load(url: String?) {
-        let url = URL(string: url!)
-        self.kf.setImage(with: url)
+    func load(url: String) {
+        let url = URL(string: url)
+        self.kf.setImage(with: url) { result in
+            switch result {
+            case .success(_):
+                break
+            case .failure(_):
+                self.image = UIImage(named: "LoadFail.jpg")
+            }
+        }
     }
 }
