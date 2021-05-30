fc8e2e3fba9c136aa26694d9ba1f59fdd7bf5ca4
Hannah Park <ha3158987@gmail.com>
2021-05-30 16:30:41 +0900

    feat: RoomPriceModal 기본 UI 구현 (#69)

---
+++ frontend/src/components/SearchBar/RoomPrice/PriceRange.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function PriceRange() {
+    return (
+        <PriceRangeLayout>
+        <div>
+            
+        </div>
+        </PriceRangeLayout>
+    )
+}
+
+const PriceRangeLayout = styled.div`
+    border: 1px solid blue;
+`
+
+export default PriceRange;
--- frontend/src/components/SearchBar/RoomPrice/RoomPriceModal.tsx
+++ frontend/src/components/SearchBar/RoomPrice/RoomPriceModal.tsx
@@ -1,15 +1,24 @@
+import PriceRange from 'components/SearchBar/RoomPrice/PriceRange';
 import styled from "styled-components";
 import { roomPriceClickState, searchBarClickState } from "recoil/Atoms";
 import { useRecoilValue, useSetRecoilState } from "recoil";
+
 const RoomPriceModal = () => {
   const isClicked = useRecoilValue(roomPriceClickState);
-  return isClicked ? <ModalLayout>데이지~~</ModalLayout> : <></>;
+  return isClicked
+  ? <ModalLayout>
+    {/* 데이지 최고~~~~~~~ */}
+    <div className="price-modal-title">가격 범위</div>
+    <div className="price-range">₩100,000 ~ 1,000,000+</div>
+    <div className="price-average">평균 1박 요금은 ₩{}입니다.</div>
+    <PriceRange/>
+    </ModalLayout>
+  : <></>;
 };
 const ModalLayout = styled.div`
   position: absolute; //relative는 SearchBarLayout 입니다
   top: 100%;
-  width: 493px;
-  height: 364px;
+  /* width: 450px; */
   right: 0;
   z-index: 1;
   overflow: hidden;
@@ -18,8 +27,30 @@ const ModalLayout = styled.div`
   box-shadow: rgb(0 0 0 / 20%) 0px 6px 20px;
   border-radius: 40px;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: left;
   flex-direction: column;
+  padding: 4rem;
+
+  & > div {
+    width: 35rem;
+    padding: 2rem;
+    color: #555;
+  }
+
+  .price-modal-title {
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+
+  .price-range {
+    font-size: 1.5rem;
+    padding-bottom: 0;
+  }
+
+  .price-average {
+    padding-top: 1rem;
+    font-size: 1.3rem;
+    color: #979292b3;
+  }
 `;
 export default RoomPriceModal;
