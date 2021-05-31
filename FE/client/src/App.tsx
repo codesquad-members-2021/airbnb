cbe013c2abf9db487bccc5d46e75f8efe047f23c
jeonyeonkyu <ehb6915@naver.com>
2021-05-31 16:39:08 +0900

    Feat: React.lazy 추가

--- FE/client/src/App.tsx
+++ FE/client/src/App.tsx
@@ -1,18 +1,30 @@
+import React, { Suspense } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline'; //https://material-ui.com/components/css-baseline/
 import { RecoilRoot } from 'recoil';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from '@Components/Pages/MainPage';
-import SearchMapPage from '@Components/Pages/SearchMapPage';
-import Page404 from '@Components/Pages/Page404';
+
+const SearchMapPageLazy = React.lazy(() => import('@Components/Pages/SearchMapPage'));
+const Page404Lazy = React.lazy(() => import('@Components/Pages/Page404'));
 
 const App = () => {
   return (
     <RecoilRoot>
       <CssBaseline />
       <Switch>
-        <Route exact path="/searchMap" component={SearchMapPage} />
-        <Route exact path="/"  component={MainPage} />
-        <Route component={Page404} />
+        <Route exact path="/searchMap">
+          <Suspense fallback="loading...">
+            <SearchMapPageLazy />
+          </Suspense>
+        </Route>
+        
+        <Route exact path="/" component={MainPage} />
+
+        <Route>
+          <Suspense fallback="loading...">
+            <Page404Lazy />
+          </Suspense>
+        </Route>
       </Switch>
     </RecoilRoot>
   );
