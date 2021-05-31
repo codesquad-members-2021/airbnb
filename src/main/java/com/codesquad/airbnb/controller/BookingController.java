760aa18c44c5b2b878ed5adbfdb7d99715d179de
bibi <bibi6666667@kakao.com>
2021-05-31 15:47:23 +0900

    refactor : 불필요한 주석 제거 #22

--- src/main/java/com/codesquad/airbnb/controller/BookingController.java
+++ src/main/java/com/codesquad/airbnb/controller/BookingController.java
@@ -29,10 +29,6 @@ public class BookingController {
     }
 
     private boolean isAvailableBooking(Booking booking) {
-        //  i) 예약 내역이 전혀 없는 경우.
-        //        - roomId에 해당하는 booking 있는지 조회
-        //        - -> 모든 booking에 room이 없으면(hasBooking이 false) true (= 예약 가능)
-        //        - -> 모든 booking에 room 하나라도 있는게 확인 되면 아래 작업 진행
         List<Booking> allBooking = bookingRepository.findAll();
         boolean hasBooking = false;
         for (Booking oneBooking : allBooking) {
@@ -45,13 +41,6 @@ public class BookingController {
             System.out.println("예약 가능 - 이 방에 한 번도 예약된 적이 없어요.");
             return true;
         }
-        //-- repository에서..
-        //    - booking에 room을 join
-        //    - ii) 예약이 있다면 그 예약이 ~6/1 전에 끝나야.
-        //        - checkOut ?
-        //    - iii) 예약이 있다면 그 예약이 6/3~ 이후에 시작해야.
-        //        - checkIn (not between 아님)
-        //
         //-- controller에서..
         //    -> 날짜 겹침 -> List not empty -> "false" (= 예약 불가능)
         //    -> 날짜 겹치지 않음 -> List empty -> "true" (= 예약 가능)
