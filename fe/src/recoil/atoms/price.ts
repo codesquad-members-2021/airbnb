df6b596669ac9154243d7937960d9cd6536d7d84
SEONKYU\difke <somedaycode@gmail.com>
2021-06-02 03:25:38 +0900

    Fix: 배포된 host로 주소 변경

--- fe/src/recoil/atoms/price.ts
+++ fe/src/recoil/atoms/price.ts
@@ -44,7 +44,9 @@ const priceList = selector({
   key: 'priceList',
   // 추후 체크인, 체크아웃을 쿼리에 붙인다.
   get: async () => {
-    const res = await fetch('http://3.35.3.106:8080/accomodation/price');
+    const res = await fetch(
+      'http://3.35.178.32:8080/accomodation/prices/period?check_in=2021-01-01&check_out=2021-12-31'
+    );
     const data = await res.json();
     return await delay(3000, data);
   },
