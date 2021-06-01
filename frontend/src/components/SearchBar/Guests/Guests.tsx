096fb575b659c14d941b283bd239696f25c05329
Hannah Park <ha3158987@gmail.com>
2021-06-01 16:31:00 +0900

    feat: 인원모달 구현 완료 (#78) - 맥시멈 인원 설정 - 성인이 없을 시 어린이 및 유아 추가 불가 조건 추가 - 서치바에 실실간으로 추가인원 노출

--- frontend/src/components/SearchBar/Guests/Guests.tsx
+++ frontend/src/components/SearchBar/Guests/Guests.tsx
@@ -1,12 +1,19 @@
 import GuestsModal from "./GuestsModal";
 import * as S from "components/SearchBar/SearchBarStyles";
 import CancelButton from "components/common/CancelButton";
-import { guestsClickState, searchBarClickState } from "recoil/Atoms";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { guestState, guestsClickState, searchBarClickState } from "recoil/Atoms";
+import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
 import { search } from "util/enum";
 const Guests = () => {
   const setsSearchBarClick = useSetRecoilState(searchBarClickState);
+  const [ guests, setGuests ] = useRecoilState(guestState);
   const isClicked = useRecoilValue(guestsClickState);
+
+  const getTotalGuestData = () => {
+    if (!guests.adult && !guests.child && !guests.toddler) return "게스트 추가";
+    else if(!guests.toddler) return `게스트 ${guests.adult + guests.child}명`;
+    else return `게스트 ${guests.adult + guests.child}명, 유아 ${guests.toddler}명`;
+  }
 
   return (
     <>
@@ -19,7 +26,7 @@ const Guests = () => {
         }}
       >
         <S.SearchBarTitle>인원</S.SearchBarTitle>
-        <S.SearchBarText _overFlow={true}>게스트 추가</S.SearchBarText>
+        <S.SearchBarText _overFlow={true}>{getTotalGuestData()}</S.SearchBarText>
         <CancelButton _right="30%" type={search.guests} />
       </S.SearchBarBox>
       {isClicked && <GuestsModal />}
