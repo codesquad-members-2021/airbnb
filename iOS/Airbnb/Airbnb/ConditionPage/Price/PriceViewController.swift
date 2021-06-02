9df36108c6c82bdeef4cb3852ec8e202c75a87a7
Lia <judykim257@gmail.com>
2021-06-02 14:34:10 +0900

    feat: ✨PriceView 파일 생성(#42)

---
+++ iOS/Airbnb/Airbnb/ConditionPage/Price/PriceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PriceViewController.swift
+//  Airbnb
+//
+//  Created by Lia on 2021/06/02.
+//
+
+import UIKit
+
+class PriceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
