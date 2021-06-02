2a4faeabb0f608729d2bfaec040764ef39ce691a
Jungwon Lee <leejungwon96@gmail.com>
2021-06-02 11:09:50 +0900

    Feat. 동이 준 Home쪽 API로 데이터 뿌려두기 -d-h-k/airbnb/#56

--- FE/airbnb-fe/src/components/Home/TourSection/CityTour/index.jsx
+++ FE/airbnb-fe/src/components/Home/TourSection/CityTour/index.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import CityCard from './CityCard';
-import cityData from '../../../utils/mock/city-card.json';
+// import cityData from '../../../utils/mock/city-card.json';
+import API from '../../../utils/API';
+
 const CityTour = () => {
+  const [cityData, setCityData] = useState();
+
+  useEffect(() => {
+    const fetchData = API.get.home();
+    fetchData.then((res) => setCityData(res.recommendedList));
+  });
   return (
     <CityTourDiv>
       <CityTitle>가까운 여행지 둘러보기</CityTitle>
       <CityCardFrame>
-        {cityData.map((cityCard, idx) => (
+        {cityData?.map((cityCard, idx) => (
           <CityCard
             key={idx}
-            cityImg={cityCard.imgUrl}
-            cityName={cityCard.name}
-            cityDistance={cityCard.distance}
+            cityImg={cityCard.image}
+            cityName={cityCard.cityName}
+            cityDistance={cityCard.description}
           ></CityCard>
         ))}
       </CityCardFrame>
--- FE/airbnb-fe/src/components/Home/TourSection/index.jsx
+++ FE/airbnb-fe/src/components/Home/TourSection/index.jsx
@@ -1,14 +1,15 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CityTour from './CityTour';
 import RoomTour from './RoomTour';
 import API from '../../utils/API';
 
 const TourSection = () => {
-  // const [homeData, setHomeData] = useState();
-  // useEffect(() => {
-  //   setHomeData(API.get.home());
-  // }, []);
-
+  const [homeData, setHomeData] = useState();
+  useEffect(() => {
+    API.get.home().then((res) => setHomeData(res));
+  }, []);
+  console.log('홈데이터', homeData);
   return (
     <TourSectionDiv>
       <CityTour></CityTour>
