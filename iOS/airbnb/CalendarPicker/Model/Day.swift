bd711a8333b02dba6d9937a7826f7ec7e0657e7c
ellyheetov <ellyheetov@gmail.com>
2021-06-02 17:26:10 +0900

    feat:지난 날짜는 선택 불가능 옵션 추가

--- iOS/airbnb/CalendarPicker/Model/Day.swift
+++ iOS/airbnb/CalendarPicker/Model/Day.swift
@@ -10,6 +10,6 @@ import Foundation
 struct Day {
     let date: Date // Date represents a given day in a month.
     let number: String // The number to display on the collection view cell.
-    let isSelected: Bool // Keeps track of whether this date is selected.
+    var isSelected: Bool // Keeps track of whether this date is selected.
     let isWithinDisplayedMonth: Bool // Tracks if this date is within the currently-viewed month.
 }
--- iOS/airbnb/CalendarPicker/ViewModel/CalendarViewModel.swift
+++ iOS/airbnb/CalendarPicker/ViewModel/CalendarViewModel.swift
@@ -21,6 +21,7 @@ class CalendarViewModel {
         }
         if !day.isWithinDisplayedMonth {
             cell.numberLabel.textColor = .systemGray
+            cell.isUserInteractionEnabled = false
         }
         cell.numberLabel.text = day.number
     }
@@ -29,6 +30,7 @@ class CalendarViewModel {
         guard let cell = cell as? CalendarDateCell else {
             return
         }
+        day.isSelected = cell.isSelected
         cell.selectionBackgroundView.backgroundColor = cell.isSelected ? .systemBlue : .clear
     }
     
