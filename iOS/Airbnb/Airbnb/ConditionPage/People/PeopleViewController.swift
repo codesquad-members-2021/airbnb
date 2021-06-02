bd9f46b512848ffa89ac213e679a67e59efde9ff
Lia <judykim257@gmail.com>
2021-06-02 16:15:51 +0900

    feat: ✨people 화면 골격 생성(#42)

--- iOS/Airbnb/Airbnb/ConditionPage/People/PeopleViewController.swift
+++ iOS/Airbnb/Airbnb/ConditionPage/People/PeopleViewController.swift
@@ -1,19 +1,16 @@
 //
-//  PriceViewController.swift
+//  PeopleViewController.swift
 //  Airbnb
 //
 //  Created by Lia on 2021/06/02.
 //
 
 import UIKit
-import Combine
 
-class PriceViewController: UIViewController {
-
+class PeopleViewController : UIViewController {
     
     private var conditionViewModel: ConditionViewModel
     private var priceUseCase = PriceUseCase()
-    private var cancelBag = Set<AnyCancellable>()
     private var containerView: UIView!
     let label = UILabel()
     
@@ -29,9 +26,6 @@ class PriceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        priceUseCase.requestMainPage(condition: Condition(cityId: conditionViewModel.city, schedule: conditionViewModel.schedule, price: conditionViewModel.price, people: conditionViewModel.people))
-        bind()
-        configure()
         configureContainer()
         configureNavigation()
         configureToolBar()
@@ -40,49 +34,7 @@ class PriceViewController: UIViewController {
     
 }
 
-extension PriceViewController {
-    
-    private func bind() {
-        priceUseCase.$prices.receive(on: DispatchQueue.main)
-            .sink { prices in
-                guard let prices = prices else { return }
-                print(prices)
-            }
-            .store(in: &cancelBag)
-        
-        priceUseCase.$error
-            .receive(on: DispatchQueue.main)
-            .sink { error in
-                guard let error = error else { return }
-                print(error) ///사용자에게 에러 표시하는 부분 미구현
-            }.store(in: &cancelBag)
-    }
-}
-
-
-extension PriceViewController {
-    
-    private func configure() {
-        view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.adjustsFontForContentSizeCategory = true
-        label.numberOfLines = 0
-        label.font = UIFont.preferredFont(forTextStyle: .title1)
-        
-        let inset = CGFloat(10)
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: inset),
-            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -inset)
-        ])
-
-    }
-    
-}
-
-
-extension PriceViewController {
+extension PeopleViewController {
     private func configureContainer() {
         containerView = UIView()
         containerView.translatesAutoresizingMaskIntoConstraints = false
@@ -102,7 +54,7 @@ extension PriceViewController {
 }
 
 
-extension PriceViewController {
+extension PeopleViewController {
     
     private func configureToolBar() {
         self.tabBarController?.tabBar.isHidden = true
@@ -118,8 +70,8 @@ extension PriceViewController {
         ])
         
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let toolbarItem1 = UIBarButtonItem(title: "건너뛰기", style: .plain, target: self, action: #selector(nextButtonPressed))
-        let toolbarItem2 = UIBarButtonItem(title: "다음", style: .done, target: self, action: #selector(nextButtonPressed))
+        let toolbarItem1 = UIBarButtonItem(title: "건너뛰기", style: .plain, target: self, action: nil)
+        let toolbarItem2 = UIBarButtonItem(title: "다음", style: .done, target: self, action: nil)
 
         let items = [toolbarItem1, flexibleSpace, toolbarItem2]
         items.forEach{ $0.tintColor = .black }
@@ -134,10 +86,4 @@ extension PriceViewController {
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
     
-    @objc func nextButtonPressed() {
-        let nextVC = PeopleViewController(conditionViewModel: conditionViewModel)
-        self.navigationController?.pushViewController(nextVC, animated: true)
-    }
-    
 }
-
