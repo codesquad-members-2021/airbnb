caae4c16f04e0eeaa46b1f1999f42d07caddc5c6
junamee <skawn11114@gmail.com>
2021-06-03 21:17:18 +0900

    Style. 예약화면 구성

--- frontend/src/components/reservation/Reservation.tsx
+++ frontend/src/components/reservation/Reservation.tsx
@@ -1,12 +1,76 @@
 import styled from 'styled-components'
+import { useRecoilValue } from 'recoil'
+import { FilterDateToString } from '../../customHook/useDateInfo'
+import {PersonnelBlock} from '../searchBar/personnel/Personnel'
+import Date from '../searchBar/date/Date'
+import {CheckInBlock} from '../searchBar/date/CheckIn'
+import {CheckOutBlock} from '../searchBar/date/CheckOut'
+import { BarBlock, BarInnerWrapper, BarTitle, BarMessage } from '../../style/BarStyle'
+import {personnelMessage,checkInMessage,checkOutMessage} from '../../customHook/atoms'
 interface IReserve{
-  handleModalClick: ()=>void
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  targetData: any
 }
-function Reservation({handleModalClick}:IReserve) {
+interface IpriceType {
+  type?:string
+}
+
+function Reservation({setOpen, targetData}:IReserve) {
+  const checkIn=useRecoilValue(checkInMessage)
+  const checkOut=useRecoilValue(checkOutMessage)
+  const guestMsg=useRecoilValue(personnelMessage)
+  console.log(targetData)
+  const handleModalClick = () =>{
+    setOpen(false)
+  }
   return (<BackgroundBlock className="MODAL" onClick={handleModalClick}>
-    <ReservationBlock onClick={(e)=>e.stopPropagation()}/>
+    <ReservationBlock onClick={(e)=>e.stopPropagation()}>
+      <FlexBox>
+        <PriceDay type='modal'>
+          ₩ {targetData.price_per_date}<span>/ 박</span>
+        </PriceDay>
+        <ReviewSpan>후기 {targetData.review.review}개</ReviewSpan>
+      </FlexBox>
+      <GridBox>
+        <Date type='modal'/>
+        {/* {CheckInBlock(checkIn)}
+        {CheckOutBlock(checkOut)}
+        {PersonnelBlock(guestMsg)} */}
+      </GridBox>
+    </ReservationBlock>
   </BackgroundBlock>)
 }
+const GridBox = styled.div`
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+border: 1px solid ${({theme})=>theme.color.black};
+border-radius: 20px;
+// div{
+//   padding: 10px;
+//   border-right: 1px solid black;
+//   border-bottom: 1px solid black;
+//   &:nth-child(2){
+//     border-right: none;
+//   }
+//   &:nth-child(3){
+//     border-bottom: none;
+//   }
+//   div{
+//     border: none;
+//     padding: 0;
+//   }
+}`
+const FlexBox= styled.div`
+display: flex;
+justify-content: space-between;
+margin-bottom: 10px;`
+const ReviewSpan = styled.span`
+display: inline-block;
+color: ${({ theme }) => theme.color.grey_2};
+font-weight: ${({ theme }) => theme.fontWeight.w1};
+text-decoration: underline;
+cursor: pointer;
+`
 const BackgroundBlock = styled.div`
   position: fixed;
   right:0;
@@ -24,5 +88,15 @@ const ReservationBlock = styled.div`
   position: absolute;
   left:40vw;
   top:30vh;
+  padding: 24px;
+`
+const PriceDay = styled.div<IpriceType>`
+  font-weight: ${({ theme }) => theme.fontWeight.w2};
+  font-size: ${({type, theme}) => type==='column'? theme.fontSize.x_sm: theme.fontSize.lg};
+  align-self: flex-end;  
+  span {
+      font-weight: ${({ theme }) => theme.fontWeight.w1};
+      font-size: ${({ theme }) =>theme.fontSize.x_sm};
+    }
 `
 export default Reservation
