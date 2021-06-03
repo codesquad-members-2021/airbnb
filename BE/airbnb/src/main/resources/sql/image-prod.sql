d4a3c8b2e37f4886e7f24852db27cbdd1d440667
janeljs <jisunlim818@gmail.com>
2021-06-04 07:11:14 +0900

    feat: Remove csv files (#23)

--- BE/airbnb/src/main/resources/sql/image-prod.sql
+++ BE/airbnb/src/main/resources/sql/image-prod.sql
@@ -1,8 +1,6 @@
-# set global local_infile=1;
-
 LOAD DATA LOCAL INFILE '/home/ubuntu/csv/image.csv'
-    INTO TABLE image FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n'
-    (@var1, @var2, @var3)
+    INTO TABLE image FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'
+    (@var1, @var2)
     SET
         room_id = @var1,
         image = @var2;
