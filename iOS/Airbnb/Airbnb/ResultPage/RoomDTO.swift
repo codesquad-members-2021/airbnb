89a2ba59d0cb90c1fb7112e55b5d6bccfcc16171
Lia <judykim257@gmail.com>
2021-06-03 21:58:57 +0900

    feat: ✨accommodation network usecase & DTO 생성(#45)

---
+++ iOS/Airbnb/Airbnb/ResultPage/RoomDTO.swift
@@ -0,0 +1,55 @@
+//
+//  RoomDTO.swift
+//  Airbnb
+//
+//  Created by Lia on 2021/06/03.
+//
+
+import Foundation
+
+struct Rooms: Codable, Hashable {
+    var rooms : [Room]
+}
+
+struct Room: Codable, Hashable {
+    var roomId: Int
+    var images: Images
+    var price: Int
+    var people: Int
+    var title: String
+    var description: String
+    var host: Host
+    var detail: Detail
+    var grade: Int
+    var review: Int
+    var tax: Tax
+    
+    struct Images: Codable, Hashable {
+        var mainImage: String
+        var detailImage: [String]
+    }
+    
+    struct Host: Codable, Hashable {
+        var image: String
+        var name: String
+    }
+    
+    struct Detail: Codable, Hashable {
+        var maxPeople: Int
+        var oneRoom: Bool
+        var bed: Int
+        var bath: Int
+        var hairDryer: Bool
+        var airConditioner: Bool
+        var WiFi: Bool
+        var kitchen: Bool
+    }
+    
+    struct Tax: Codable, Hashable {
+        var cleanTax: Int
+        var serviceTax: Int
+        var accommodationTax: Int
+    }
+}
+
+
