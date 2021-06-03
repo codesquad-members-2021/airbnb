5c5fe7ddb7d9444bd96af5aa827dc4c2b8a941bf
Lia <judykim257@gmail.com>
2021-06-03 21:45:56 +0900

    wip: ⚙️숙소 검색 뷰 골격 생성(#45)

--- iOS/Airbnb/Airbnb/ResultPage/SearchResultViewController.swift
+++ iOS/Airbnb/Airbnb/ResultPage/SearchResultViewController.swift
@@ -6,12 +6,141 @@
 //
 
 import UIKit
+import Combine
 
 class SearchResultViewController: UIViewController {
 
+    static let headerElementKind = "header-element-kind"
+    
+    private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Int>!
+    private let mainUseCase = MainPageUseCase()
+    private var cancelBag = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
 }
+
+
+extension SearchResultViewController {
+    
+    private func bind() {
+        mainUseCase.$mainPage.receive(on: DispatchQueue.main)
+            .sink { mainPage in
+                guard let mainPage = mainPage else { return }
+                self.applyInitialSnapshots(with: mainPage)
+            }
+            .store(in: &cancelBag)
+        
+        mainUseCase.$error
+            .receive(on: DispatchQueue.main)
+            .sink { error in
+                guard let error = error else { return }
+                print(error) ///사용자에게 에러 표시하는 부분 미구현
+            }.store(in: &cancelBag)
+    }
+    
+}
+
+
+extension SearchResultViewController {
+    
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 10
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .estimated(44)),
+                elementKind:SearchResultViewController.headerElementKind,
+                alignment: .top)
+            section.boundarySupplementaryItems = [sectionHeader]
+            
+            return section
+        }
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: collectionView) {
+            (collectionView, indexPath, item) -> UICollectionViewCell? in
+
+                self.collectionView.register(AccommodationCell.nib, forCellWithReuseIdentifier: AccommodationCell.reuseIdentifier)
+                let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: AccommodationCell.reuseIdentifier, for: indexPath) as! AccommodationCell
+//                cell.fillUI(with: item.city ?? City(id: 0, name: "", image: "", distance: 0))
+                return cell
+        }
+        self.configureSupplementaryView()
+    }
+    
+    private func configureSupplementaryView() {
+        let supplementaryRegistration = UICollectionView.SupplementaryRegistration
+        <ResultSupplementaryView>(elementKind: SearchResultViewController.headerElementKind) {
+            (supplementaryView, string, indexPath) in
+//            let sectionKind = Section(rawValue: indexPath.section)!
+            supplementaryView.label.text = "~~~"//String(describing: sectionKind)
+        }
+        dataSource.supplementaryViewProvider = { (view, kind, index) in
+            return self.collectionView.dequeueConfiguredReusableSupplementary(
+                using: supplementaryRegistration, for: index)
+        }
+    }
+    
+    private func applyInitialSnapshots(with mainPage: MainPage) {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([1])
+        snapshot.appendItems([1], toSection: 1)
+        dataSource.apply(snapshot)
+    }
+    
+}
+
+extension SearchResultViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailViewController = DetailDestinationViewController()
+        self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let indexPath = self.collectionView.indexPathsForSelectedItems?.first else { return }
+        if let coordinator = self.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { context in
+                self.collectionView.deselectItem(at: indexPath, animated: true)
+            }) { (context) in
+                if context.isCancelled {
+                    self.collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+                }
+            }
+        } else {
+            self.collectionView.deselectItem(at: indexPath, animated: animated)
+        }
+    }
+    
+}
+
