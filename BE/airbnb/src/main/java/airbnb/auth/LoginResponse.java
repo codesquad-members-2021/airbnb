65d3d0f21b77d570057b195953980255b765dfa2
janeljs <jisunlim818@gmail.com>
2021-06-04 07:19:13 +0900

    feat: Add LoginResponse (#91)

---
+++ BE/airbnb/src/main/java/airbnb/auth/LoginResponse.java
@@ -0,0 +1,13 @@
+package airbnb.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String jwt;
+    private String profileUrl;
+}
