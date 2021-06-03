e79a82f30cf25e888f99ea80283854d676764a08
Lia <judykim257@gmail.com>
2021-06-04 01:04:40 +0900

    feat: ✨tabBar item 추가(#45)

--- iOS/Airbnb/Airbnb/SupportingFiles/SceneDelegate.swift
+++ iOS/Airbnb/Airbnb/SupportingFiles/SceneDelegate.swift
@@ -13,7 +13,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        configureTabBar()
     }
 
+    func configureTabBar() {
+        guard let tabBarController = self.window?.rootViewController as? UITabBarController else { return }
+        
+        tabBarController.tabBar.tintColor = .black
+        tabBarController.tabBar.barTintColor = .systemGray6
+        
+        if let barItems = tabBarController.tabBar.items {
+            barItems[0].image = UIImage(systemName: "magnifyingglass")
+            barItems[1].image = UIImage(systemName: "heart")
+            barItems[2].image = UIImage(systemName: "person")
+            
+            barItems[0].title = "검색"
+            barItems[1].title = "위시리스트"
+            barItems[2].title = "로그인"
+            
+        }
+    }
+    
 }
 
