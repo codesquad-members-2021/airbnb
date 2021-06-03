a2726c846b76b7e932461d489e04e0d650f7813b
Lia <judykim257@gmail.com>
2021-06-04 02:35:38 +0900

    feat: ✨detail Room view 구현(#45)

---
+++ iOS/Airbnb/Airbnb/ResultPage/ViewController/DetailRoomViewControllerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailRoomViewControllerViewController.swift
+//  Airbnb
+//
+//  Created by Lia on 2021/06/04.
+//
+
+import UIKit
+
+class DetailRoomViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
