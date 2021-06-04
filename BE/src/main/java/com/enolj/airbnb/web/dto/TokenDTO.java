1f76bf80d192d65d33e11a84da3bf63307b163f8
jeong-inho <derosatam76@gmail.com>
2021-06-04 15:20:49 +0900

    Refactor: [#90] TokenDTO 클래스의 token_type 멤버변수를 tokenType으로 변경

--- BE/src/main/java/com/enolj/airbnb/web/dto/TokenDTO.java
+++ BE/src/main/java/com/enolj/airbnb/web/dto/TokenDTO.java
@@ -6,15 +6,16 @@ public class TokenDTO {
 
     @JsonProperty("access_token")
     private String accessToken;
-    private String token_type;
+    @JsonProperty("token_type")
+    private String tokenType;
     private String scope;
 
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
     }
 
-    public void setToken_type(String token_type) {
-        this.token_type = token_type;
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
     }
 
     public void setScope(String scope) {
@@ -29,7 +30,7 @@ public class TokenDTO {
     public String toString() {
         return "TokenDTO{" +
                 "access_token='" + accessToken + '\'' +
-                ", token_type='" + token_type + '\'' +
+                ", token_type='" + tokenType + '\'' +
                 ", scope='" + scope + '\'' +
                 '}';
     }
