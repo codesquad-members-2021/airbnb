5422d50749173a3d1e8d2b758b4b3e2803d575c2
Lia <judykim257@gmail.com>
2021-06-04 12:05:47 +0900

    feat: ✨지도 뷰 파일 생성(#47)

---
+++ iOS/Airbnb/Airbnb/ResultPage/ViewController/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Airbnb
+//
+//  Created by Lia on 2021/06/04.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
