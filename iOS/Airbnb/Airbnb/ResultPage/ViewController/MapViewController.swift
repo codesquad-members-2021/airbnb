0ad95b083892f2f0acea41a70fb0245f2907bd95
Lia <judykim257@gmail.com>
2021-06-04 13:32:24 +0900

    feat: ✨지도 뷰 화면 띄우기(#47)

--- iOS/Airbnb/Airbnb/ResultPage/ViewController/MapViewController.swift
+++ iOS/Airbnb/Airbnb/ResultPage/ViewController/MapViewController.swift
@@ -6,13 +6,81 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var pointAnnotation: MKPointAnnotation!
+    var pinAnnotationView: MKPinAnnotationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         
+        setAnnotation(latitude: 37.3826616, longitude: 126.840719, delta: 0.5)
+        setAnnotation(latitude: 37.6447360, longitude: 127.005575, delta: 0.5)
     }
+    
+}
 
 
+extension MapViewController: MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    private func configure() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+    }
+    
+    private func setAnnotation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta span :Double) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = moveToLocation(latitude: latitude, longtude: longitude, delta: span)
+        mapView.addAnnotation(annotation)
+    }
+    
+    private func moveToLocation(latitude: CLLocationDegrees, longtude: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
+        let location = CLLocationCoordinate2DMake(latitude, longtude)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let region = MKCoordinateRegion(center: location, span: spanValue)
+        mapView.setRegion(region, animated: true)
+        return location
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "label"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            annotationView?.canShowCallout = true
+        } else {
+            annotationView?.annotation = annotation
+        }
+        annotationView?.addSubview(configureLabel())
+        
+        return annotationView
+    }
+    
+    private func configureLabel() -> UILabel {
+        let label = UILabel()
+        label.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 60, height: 40))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(greaterThanOrEqualTo: label.heightAnchor, multiplier: 1, constant: 80).isActive = true
+        label.heightAnchor.constraint(greaterThanOrEqualTo: label.heightAnchor, multiplier: 1).isActive = true
+        label.layer.cornerRadius = 5
+        label.layer.masksToBounds = true
+        label.backgroundColor = .white
+        label.textAlignment = .center
+        label.text = "₩201,599"
+        
+        return label
+    }
+    
 }
--- iOS/Airbnb/Airbnb/SupportingFiles/SceneDelegate.swift
+++ iOS/Airbnb/Airbnb/SupportingFiles/SceneDelegate.swift
@@ -12,7 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+                self.window = UIWindow(windowScene: windowScene)
+
+                let navigationController = UINavigationController.init(rootViewController: MapViewController())
+                self.window?.rootViewController = navigationController
+
+                self.window?.rootViewController = navigationController
+                self.window?.makeKeyAndVisible()
         configureTabBar()
     }
 
