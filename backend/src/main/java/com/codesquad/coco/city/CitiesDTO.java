6248c4c49658351305b122ea5c5d3bcd9ae2e755
gisungchoi <chlrltjd159@naver.com>
2021-06-04 14:18:53 +0900

    feat : city관련 수정 사항 변경
    - null을 예방하기 위해 초기화
    - final 붙여주기
    - requestMapping 사용

--- backend/src/main/java/com/codesquad/coco/city/CitiesDTO.java
+++ backend/src/main/java/com/codesquad/coco/city/CitiesDTO.java
@@ -1,10 +1,11 @@
 package com.codesquad.coco.city;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CitiesDTO {
 
-    private List<CityDTO> cities;
+    private List<CityDTO> cities = new ArrayList<>();
 
     public CitiesDTO(List<CityDTO> city) {
         this.cities = city;
