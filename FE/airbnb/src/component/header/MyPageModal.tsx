de64fd57e6d2170fb1dac5f6043da324b80c1489
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-06 00:36:20 +0900

    fix: 상위에서 Props로 받았던 이벤트핸들러를 Props로 받지 않고 import

--- FE/airbnb/src/component/header/MyPageModal.tsx
+++ FE/airbnb/src/component/header/MyPageModal.tsx
@@ -1,15 +1,12 @@
 import styled from "styled-components";
+import { stopPropagation } from "component/searchBar/modalFunctions";
 
-interface Props {
-  onClick: (e: React.MouseEvent) => void;
-}
-
-function MyPageModal({ onClick }: Props) {
+function MyPageModal() {
   const clientID = process.env.REACT_APP_CLIENT_ID;
   const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=http://localhost:3000/api/login/callback&scope=user`;
 
   return (
-    <ModalContainer onClick={onClick}>
+    <ModalContainer onClick={stopPropagation}>
       <a href={LOGIN_URL}>로그인</a>
     </ModalContainer>
   );
