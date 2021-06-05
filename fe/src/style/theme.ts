71dffc3545be3040e28214a26ea91c0a40380782
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-06-05 16:52:12 +0900

    refactor: theme 추가

--- fe/src/style/theme.ts
+++ fe/src/style/theme.ts
@@ -1,13 +1,28 @@
 import "styled-components";
 
-
 interface DefaultTheme {
-  [key: string]: {
+  fontFamily: string;
+  fontWeight: {
+    [key: string]: string;
+  };
+  color: {
+    [key: string]: string;
+  };
+  fontSize: {
+    [key: string]: string;
+  };
+  border: {
     [key: string]: string;
   };
 }
 
 const theme: DefaultTheme = {
+  fontFamily: "Noto Sans KR",
+  fontWeight: {
+    normal: "400",
+    bold: "700",
+    bold2: "900",
+  },
   color: {
     White: " #FFFFFF",
     Gray6: "#F5F5F7",
@@ -20,11 +35,18 @@ const theme: DefaultTheme = {
     Red: "#E84C60",
     Green: "#118917",
   },
+  fontSize: {
+    S: "12px",
+    M: "14px",
+    L: "16px",
+    XL: "18px",
+    XXL: "20px",
+  },
+
   border: {
     Gray4: "1px solid #BDBDBD",
     Gray5: "1px solid rgb(221, 221, 221)",
   },
 };
-
 
 export { theme };
